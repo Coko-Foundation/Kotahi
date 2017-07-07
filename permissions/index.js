934fb427b7b136772a272a8af660f26870581d33
Alf Eaton <eaton.alf@gmail.com>
2017-07-07 13:29:32 +0100

    Add some example permissions

--- permissions/index.js
+++ permissions/index.js
@@ -1,1 +1,20 @@
-module.exports = (user, operation, object) => true // TODO
+module.exports = (user, operation, project, version) => {
+  return true // TODO
+
+  // switch (operation) {
+  //   case 'publish':
+  //     return version.decision &&
+  //       !version.published &&
+  //       user.admin
+  //
+  //   case 'decision':
+  //     return version.reviews &&
+  //       !version.decision &&
+  //       version.roles.editor.find(role => role.user.id === user.id)
+  //
+  //   case 'accept_invitation':
+  //     return version.invitations.reviewer
+  //       .filter(invitation => !invitation.accepted)
+  //       .find(invitation => invitation.user.email === user.email)
+  // }
+}
