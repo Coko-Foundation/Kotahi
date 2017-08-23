b48e61b100c9d3a6315ce66a982d1cac1aa7c1ca
Alf Eaton <eaton.alf@gmail.com>
2017-08-23 13:33:38 +0100

    Remove unused utility libraries

--- packages/component-submit/src/lib/sort.js
+++
@@ -1,3 +0,0 @@
-export const ascending = field => (a, b) => a[field] - b[field]
-
-export const descending = field => (a, b) => b[field] - a[field]
--- packages/component-submit/src/lib/text.js
+++
@@ -1,1 +0,0 @@
-export const ucfirst = (text) => text.substr(0, 1).toUpperCase() + text.substr(1)
