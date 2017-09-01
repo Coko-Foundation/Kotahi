1a299447b23655f8277206cd43f4fea3ac685ff6
Alf Eaton <eaton.alf@gmail.com>
2017-09-01 10:02:32 +0100

    Use div instead of SVG

--- packages/component-submit/src/components/Validot.js
+++ packages/component-submit/src/components/Validot.js
@@ -7,16 +7,13 @@ import classes from './Validot.local.css'
 const scrollIntoView = id => document.getElementById(id).scrollIntoView()
 
 const Validot = ({ input, meta }) => (
-  <svg
+  <div
     className={classnames(classes.root, {
       [classes.valid]: meta.valid,
       [classes.error]: meta.error,
       [classes.warning]: meta.warning,
     })}
-    onClick={() => scrollIntoView(input.name)}
-    viewBox="0 0 40 40">
-    <circle cx="20" cy="20" r="20"/>
-  </svg>
+    onClick={() => scrollIntoView(input.name)}/>
 )
 
 export default Validot
