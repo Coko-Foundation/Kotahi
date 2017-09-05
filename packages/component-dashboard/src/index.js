726bf8026bfbd4bf88309d00d2a0c19ac1a8a4ed
Alf Eaton <eaton.alf@gmail.com>
2017-09-05 23:13:45 +0100

    Remove unused reducer

--- packages/component-dashboard/src/index.js
+++ packages/component-dashboard/src/index.js
@@ -4,7 +4,6 @@ module.exports = {
       () => require('./components')
     ],
     reducers: {
-      dashboard: () => require('./redux/dashboard').default,
       manuscriptConversion: () => require('./redux/manuscriptConversion').default,
     }
   },
