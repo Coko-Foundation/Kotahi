65074cb2272f5a0ac2eb26d40cb16520f83e67db
Alf Eaton <eaton.alf@gmail.com>
2017-09-06 14:29:44 +0100

    Fix separator

--- packages/component-dashboard/src/components/sections/Divider.js
+++ packages/component-dashboard/src/components/sections/Divider.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import classes from './Divider.local.scss'
 
-export default separator => (
+export default ({ separator }) => (
   <span className={classes.root}>
-    {` ${separator} `}
+    {' ' + separator + ' '}
   </span>
 )
