6d4134bd5712a0d76e1b5e6d8f3f8484f73edc4b
Alf Eaton <eaton.alf@gmail.com>
2017-09-06 15:49:10 +0100

    Add color to recommendations

--- packages/xpub-styleguide/src/config/journal/recommendations.js
+++ packages/xpub-styleguide/src/config/journal/recommendations.js
@@ -2,13 +2,16 @@ export default [
   {
     value: 'accept',
     label: 'Accept',
+    color: 'green'
   },
   {
     value: 'revise',
     label: 'Revise',
+    color: 'orange'
   },
   {
     value: 'reject',
     label: 'Reject',
+    color: 'red'
   }
 ]
