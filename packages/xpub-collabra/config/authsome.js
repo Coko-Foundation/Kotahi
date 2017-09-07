905dd42ea8f30fcc6f845d944609355daf2aba58
Alf Eaton <eaton.alf@gmail.com>
2017-09-07 10:54:32 +0100

    Disable authsome console.log

--- packages/xpub-collabra/config/authsome.js
+++ packages/xpub-collabra/config/authsome.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: (user, operation, project, version) => {
-    console.log({ user, operation, project, version })
+    // console.log({ user, operation, project, version })
     return true // TODO
   },
   teams: {
