ff69677384723111f69cc5db853eebdcd5038d47
Alexandros Georgantas <alexgeorg86@gmail.com>
2017-09-21 17:44:31 +0300

    Use of Avatar atom in reviewer component

--- packages/component-review/src/components/reviewers/Reviewer.js
+++ packages/component-review/src/components/reviewers/Reviewer.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import classes from './Reviewer.local.scss'
+import { Avatar } from 'xpub-ui'
 
 const Reviewer = ({ reviewer, removeReviewer }) => (
   <div className={classes.root}>
     {reviewer._user && (
       <div>
+        <Avatar status={reviewer.status} />
         <div>{reviewer._user.username}</div>
         <div>{reviewer._user.email}</div>
+        <div>{`status: ${reviewer.status}`}</div>
+        <div>{`added on ${reviewer.addedOn}`}</div>
+
+        <button onClick={removeReviewer}>remove</button>
       </div>
     )}
-
-    <div>{reviewer.status}</div>
-    <div>{`added on ${reviewer.addedOn}`}</div>
-
-    <button onClick={removeReviewer}>remove</button>
   </div>
 )
 
--- packages/component-review/src/components/reviewers/Reviewer.md
+++ packages/component-review/src/components/reviewers/Reviewer.md
@@ -5,22 +5,22 @@ const user = () => ({
     id: faker.random.uuid(),
     username: faker.internet.userName(),
     email: faker.internet.email()
-})
+});
 const statusFactory = () => {
   const statuses = ['Accepted', 'Pending', 'Declined']
   return statuses[Math.floor(Math.random() * statuses.length)]
-}
-const date = () => {
+};
+const dateFactory = () => {
   const options = { year: 'numeric', month: 'long', day: 'numeric' }
   const date = new Date()
   return date.toLocaleDateString('en-US', options)
-}
+};
 const reviewer = {
     projectReviewer: faker.random.uuid(),
     _user: user(),
     status: statusFactory(),
-    addedOn: date() 
-}
+    addedOn: dateFactory() 
+};
 
 <Reviewer
   reviewer={reviewer}
