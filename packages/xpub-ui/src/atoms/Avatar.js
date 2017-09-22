57535f43773cee48454ee94ed08a2ed42c348839
Alexandros Georgantas <alexgeorg86@gmail.com>
2017-09-22 16:00:54 +0300

    Corrections for the svg. Viewbox added plus css rules

--- packages/xpub-ui/src/atoms/Avatar.js
+++ packages/xpub-ui/src/atoms/Avatar.js
@@ -10,6 +10,7 @@ const Avatar = ({ status, width, height }) => {
       <svg
         className={classes[classValue]}
         xmlns="http://www.w3.org/2000/svg"
+        viewbox={`0 0 ${width ? width + 5 : '105'} ${height ? height : '70'}`}
         width={width ? width : '100'}
         height={height ? height : '70'}
       >
