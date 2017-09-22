b38052022a813fa3b546c0c393775bde6c91faba
Alexandros Georgantas <alexgeorg86@gmail.com>
2017-09-22 17:20:37 +0300

    Refactor in the approach of resend mail btn

--- packages/component-review/src/components/reviewers/Reviewer.js
+++ packages/component-review/src/components/reviewers/Reviewer.js
@@ -3,11 +3,6 @@ import classes from './Reviewer.local.scss'
 import { Avatar, Button } from 'xpub-ui'
 
 const Reviewer = ({ reviewer, removeReviewer }) => {
-  let interactionsArea
-  if (reviewer.status && reviewer.status === 'Pending') {
-    interactionsArea = <Button onClick={removeReviewer}>RESEND MAIL</Button>
-  }
-
   return (
     <div className={classes.root}>
       {reviewer._user && (
@@ -15,7 +10,9 @@ const Reviewer = ({ reviewer, removeReviewer }) => {
           <Avatar status={reviewer.status} width="100" height="70" />
           <div className={classes.fullname}>{reviewer._user.fullname}</div>
           <div className={classes.date}>added on {reviewer.addedOn}</div>
-          {interactionsArea}
+          {reviewer.status === 'Pending' && (
+            <Button onClick={removeReviewer}>RESEND MAIL</Button>
+          )}
         </div>
       )}
     </div>
