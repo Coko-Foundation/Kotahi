b1dbe1f161bed5f70687b7843c791ba0d17da580
Alf Eaton <eaton.alf@gmail.com>
2017-10-04 22:35:38 +0100

    Update webpack config for node-config in client

---
+++ packages/xpub-collabra/webpack/client-config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const config = require('config')
+const fs = require('fs-extra')
+const { pick } = require('lodash')
+
+const outputPath = path.resolve(__dirname, '../_build/client.json')
+
+const fields = [
+  'authsome',
+  'validations',
+  'pubsweet-client',
+  'pubsweet-server' // for API_ENDPOINT
+]
+
+fs.outputJsonSync(outputPath, pick(config, fields))
+
+module.exports = outputPath
