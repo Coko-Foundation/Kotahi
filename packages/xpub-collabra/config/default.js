111144ee993e73fe2cf709058d3aea4283116a03
Alf Eaton <eaton.alf@gmail.com>
2017-10-10 10:38:29 +0100

    Disable redux log in client

--- packages/xpub-collabra/config/default.js
+++ packages/xpub-collabra/config/default.js
@@ -19,6 +19,7 @@ module.exports = {
   'pubsweet-client': {
     'API_ENDPOINT': 'http://localhost:3000/api',
     'login-redirect': '/',
+    'redux-log': 'off',
     theme: process.env.PUBSWEET_THEME
   },
   'mail-transport': {
