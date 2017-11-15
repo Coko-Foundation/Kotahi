22051d9d186657c8f1e29795b0813668d8f69634
yannis <yannisbarlas@gmail.com>
2017-11-15 19:43:07 +0200

    remove explicit ports from client config

--- packages/xpub-collabra/config/client.js
+++ packages/xpub-collabra/config/client.js
@@ -1,6 +1,6 @@
 module.exports = {
   'pubsweet-client': {
-    'API_ENDPOINT': 'http://localhost:3000/api',
+    'API_ENDPOINT': '/api',
     'login-redirect': '/',
     'redux-log': false,
     theme: process.env.PUBSWEET_THEME
--- packages/xpub-collabra/config/default.js
+++ packages/xpub-collabra/config/default.js
@@ -21,11 +21,11 @@ module.exports = {
     sendmail: true
   },
   'password-reset': {
-    url: process.env.PUBSWEET_PASSWORD_RESET_URL || 'http://localhost:3000/password-reset',
+    url: process.env.PUBSWEET_PASSWORD_RESET_URL || '/password-reset',
     sender: process.env.PUBSWEET_PASSWORD_RESET_SENDER || 'dev@example.com'
   },
   'pubsweet-component-ink-backend': {
-    inkEndpoint: process.env.INK_ENDPOINT || 'http://ink-api.coko.foundation',
+    inkEndpoint: process.env.INK_ENDPOINT || 'http://inkdemo-api.coko.foundation',
     email: process.env.INK_USERNAME,
     password: process.env.INK_PASSWORD,
     maxRetries: 500
