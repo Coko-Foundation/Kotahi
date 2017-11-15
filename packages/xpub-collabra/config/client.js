eadce40cd6ee5df31d3a71c213b722023b193196
yannis <yannisbarlas@gmail.com>
2017-11-15 21:13:56 +0200

    revert to full url for api in config

--- packages/xpub-collabra/config/client.js
+++ packages/xpub-collabra/config/client.js
@@ -1,6 +1,6 @@
 module.exports = {
   'pubsweet-client': {
-    'API_ENDPOINT': '/api',
+    'API_ENDPOINT': 'http://localhost:3000/api',
     'login-redirect': '/',
     'redux-log': false,
     theme: process.env.PUBSWEET_THEME
