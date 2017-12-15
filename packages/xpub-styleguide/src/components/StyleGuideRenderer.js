8c905c952f749584362f36b4ecb64e8bbd5b5f9b
Tamlyn Rhodes <tamlyn@tamlyn.org>
2017-12-15 11:04:12 +0000

    Make xpub-theme and xpub-styleguide extend their @pubsweet counterparts

--- packages/xpub-styleguide/src/components/StyleGuideRenderer.js
+++ packages/xpub-styleguide/src/components/StyleGuideRenderer.js
@@ -1,18 +1,1 @@
-import React from 'react'
-import classes from './StyleGuideRenderer.local.scss'
-
-const StyleGuideRenderer = ({ title, children, toc }) => (
-  <div className={classes.root}>
-    <div className={classes.sidebar}>
-      <header className={classes.header}>
-        <h1 className={classes.title}>{title}</h1>
-      </header>
-
-      <nav className={classes.nav}>{toc}</nav>
-    </div>
-
-    <div className={classes.content}>{children}</div>
-  </div>
-)
-
-export default StyleGuideRenderer
+export { default } from '@pubsweet/styleguide/src/components/StyleGuideRenderer'
--- packages/xpub-theme/src/index.js
+++ packages/xpub-theme/src/index.js
@@ -1,8 +1,3 @@
-import 'typeface-fira-mono'
-import 'typeface-fira-sans'
-import 'typeface-fira-sans-condensed'
-import 'typeface-vollkorn'
-// import 'pubsweet-fira'
-import 'cokourier-prime-sans'
+import '@pubsweet/theme'
 
 import './variables.css'
