15f951b09d8d22b419878c5351f5077b5182f858
Aanand Prasad <aanand.prasad@gmail.com>
2018-01-17 17:25:26 +0000

    Test that lists in the dashboard display the projects

--- packages/component-dashboard/src/components/Dashboard.test.js
+++ packages/component-dashboard/src/components/Dashboard.test.js
@@ -9,6 +9,12 @@ Enzyme.configure({ adapter: new Adapter() })
 
 jest.mock('config', () => ({ 'pubsweet-client': {} }))
 
+const getProjects = section =>
+  section
+    .children()
+    .not('.heading')
+    .map(c => c.props().project)
+
 describe('Dashboard', () => {
   const makeWrapper = (props = {}) => {
     props = Object.assign(
@@ -38,26 +44,38 @@ describe('Dashboard', () => {
   })
 
   it('shows a list of projects submitted by the current user', () => {
+    const project = { id: 1 }
     const dashboard = makeWrapper({
-      dashboard: { owner: [{ id: 1 }] },
+      dashboard: { owner: [project] },
     })
+
     expect(dashboard.find('.empty')).toHaveLength(0)
-    expect(dashboard.find('.heading')).toHaveLength(1)
+    const section = dashboard.find('.section')
+    expect(section).toHaveLength(1)
+    expect(getProjects(section)).toEqual([project])
   })
 
   it('shows a list of projects to be reviewed', () => {
+    const project = { id: 1 }
     const dashboard = makeWrapper({
-      dashboard: { reviewer: [{ id: 1 }] },
+      dashboard: { reviewer: [project] },
     })
+
     expect(dashboard.find('.empty')).toHaveLength(0)
-    expect(dashboard.find('.heading')).toHaveLength(1)
+    const section = dashboard.find('.section')
+    expect(section).toHaveLength(1)
+    expect(getProjects(section)).toEqual([project])
   })
 
   it('shows a list of projects of which the current user is the editor', () => {
+    const project = { id: 1 }
     const dashboard = makeWrapper({
-      dashboard: { editor: [{ id: 1 }] },
+      dashboard: { editor: [project] },
     })
+
     expect(dashboard.find('.empty')).toHaveLength(0)
-    expect(dashboard.find('.heading')).toHaveLength(1)
+    const section = dashboard.find('.section')
+    expect(section).toHaveLength(1)
+    expect(getProjects(section)).toEqual([project])
   })
 })
