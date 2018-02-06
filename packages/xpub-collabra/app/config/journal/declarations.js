e54857918d1f46fb7a2c9a9cbf0b4a5e70e53d6f
Giannis Kopanas <jkopanas@gmail.com>
2018-02-06 17:02:51 +0200

    Changing YesNo molecule to RagioGroup which is more general and add values to configuration

--- packages/xpub-collabra/app/config/journal/declarations.js
+++ packages/xpub-collabra/app/config/journal/declarations.js
@@ -2,27 +2,87 @@ export default {
   questions: [
     {
       id: 'openData',
-      legend: 'Data is open',
+      legend: 'Data is open ?',
+      options: [
+        {
+          label: 'Yes',
+          value: 'yes',
+        },
+        {
+          label: 'No/Not Applicable',
+          value: 'no',
+        },
+      ],
     },
     {
       id: 'previouslySubmitted',
-      legend: 'Previously submitted',
+      legend: 'Previously submitted ?',
+      options: [
+        {
+          label: 'Yes',
+          value: 'yes',
+        },
+        {
+          label: 'No',
+          value: 'no',
+        },
+      ],
     },
     {
       id: 'openPeerReview',
-      legend: 'Open peer review',
+      legend: 'Open peer review ?',
+      options: [
+        {
+          label: 'Yes',
+          value: 'yes',
+        },
+        {
+          label: 'No',
+          value: 'no',
+        },
+      ],
     },
     {
       id: 'streamlinedReview',
-      legend: 'Streamlined review',
+      legend: 'Streamlined review ?',
+      options: [
+        {
+          label: 'Yes',
+          value: 'yes',
+        },
+        {
+          label: 'No',
+          value: 'no',
+        },
+      ],
     },
     {
       id: 'researchNexus',
-      legend: 'Submitted as part of the research nexus?',
+      legend: 'Submitted as part of the research nexus ?',
+      options: [
+        {
+          label: 'Yes',
+          value: 'yes',
+        },
+        {
+          label: 'No',
+          value: 'no',
+        },
+      ],
     },
     {
       id: 'preregistered',
-      legend: 'Pre-registered?',
+      legend: 'Pre-registered ?',
+      options: [
+        {
+          label: 'Yes',
+          value: 'yes',
+        },
+        {
+          label: 'No',
+          value: 'no',
+        },
+      ],
     },
   ],
 }
