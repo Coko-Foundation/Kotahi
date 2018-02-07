594aed29e9d85e1b5dcd94656cb6445ff717f53e
Tamlyn Rhodes <tamlyn@tamlyn.org>
2018-02-07 13:33:07 +0000

    feat: use login and signup components from PubSweet
    Remove "pubsweet-component-xpub-authentication"
    Temporarily add react-router-redux@5.0.0-alpha.9 to the workspace root to fix a dependency issue which should be resolved when we update to pubsweet-client>2.0.0

--- packages/component-authentication/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as PrivateRoute } from './PrivateRoute'
-export { default as LoginPage } from './LoginPage'
-export { default as LogoutPage } from './LogoutPage'
-export { default as SignupPage } from './SignupPage'
--- packages/component-authentication/src/redux/index.js
+++
@@ -1,3 +0,0 @@
-export * as currentUser from './currentUser'
-export * as login from './login'
-export * as signup from './signup'
