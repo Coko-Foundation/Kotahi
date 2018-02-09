efb39835fa9b7036ff18f28bd9eec2ca77729cc4
Giannis Kopanas <jkopanas@gmail.com>
2018-02-09 12:55:56 +0200

    Review Statuses move them to config

--- packages/component-dashboard/src/components/Reviews.js
+++ packages/component-dashboard/src/components/Reviews.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { compose, withProps } from 'recompose'
 import { groupBy } from 'lodash'
+import { withJournal } from 'xpub-journal'
 import { Badge } from '@pubsweet/ui'
 import classes from './Reviews.local.scss'
 
-const Reviews = ({ reviews }) => (
+const Reviews = ({ reviews, journal }) => (
   <div className={classes.root}>
-    {['invited', 'accepted', 'rejected', 'completed'].map(status => (
+    {journal.reviewStatus.map(status => (
       <span className={classes.badge} key={status}>
         <Badge
           count={reviews[status] ? reviews[status].length : 0}
@@ -18,6 +19,7 @@ const Reviews = ({ reviews }) => (
 )
 
 export default compose(
+  withJournal,
   withProps(props => ({
     reviews: groupBy(props.version.reviewers, 'status'),
   })),
---
+++ packages/xpub-collabra/app/config/journal/review-status.js
@@ -0,0 +1,1 @@
+export default ['invited', 'accepted', 'rejected', 'completed']
