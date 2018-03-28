844b7afc274026aa98e1655e6a8cd776fcde1a1b
Giannis Kopanas <jkopanas@gmail.com>
2018-03-28 20:34:24 +0300

    cover password from config

--- config/mailer.js
+++ config/mailer.js
@@ -3,7 +3,7 @@ module.export = {
     host: 'smtp.mailgun.org',
     auth: {
       user: 'xpub@sandbox9af3d5dc64334deca9759efe7056d6b3.mailgun.org',
-      pass: 'xpubpass',
+      pass: '',
     },
   },
 }
