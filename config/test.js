fa1c20ca5984d7f052a116b8fec9cf4ed52d8241
Giannis Kopanas <jkopanas@gmail.com>
2018-04-27 13:39:08 +0300

    fix merge rebase

--- config/test.js
+++ config/test.js
@@ -12,14 +12,10 @@ module.exports = {
       transports: [new winston.transports.Console()],
     }),
     port: 4000,
-<<<<<<< HEAD
     secret: 'secret-string',
-=======
-    secret: 'test',
     baseUrl: deferConfig(
       cfg => `http://localhost:${cfg['pubsweet-server'].port}`,
     ),
->>>>>>> fix(test): fix test config
   },
   'password-reset': deferConfig(
     cfg => `http://localhost:${cfg['pubsweet-server'].port}/password-reset`,
