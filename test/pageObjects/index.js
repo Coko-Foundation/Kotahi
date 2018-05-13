483440e48367945aeadcf9c3dda2276ca4d13dd1
ana <ana@lynx.nz>
2018-05-14 11:55:14 +1200

    feat: new submission test, added huge timeout for file upload

--- test/pageObjects/index.js
+++ test/pageObjects/index.js
@@ -1,2 +1,4 @@
 export { default as login } from './login'
 export { default as dashboard } from './dashboard'
+export { default as submission } from './submission'
+export { default as submissionInformation } from './submissionInformation'
