605dce2dbfccd4e24ccb39350498b717a1671bb8
Jure Triglav <juretriglav@gmail.com>
2020-06-08 15:52:12 +0200

    fix: re-add supplementaryfiles to form builder

--- app/components/component-xpub-formbuilder/src/components/builderComponents/Menu.js
+++ app/components/component-xpub-formbuilder/src/components/builderComponents/Menu.js
@@ -1,27 +1,36 @@
-import React from 'react'
-import { Menu, TextField, ValidatedFieldFormik } from '@pubsweet/ui'
+import React, { useState } from 'react'
+import { Select, TextField, ValidatedFieldFormik } from '@pubsweet/ui'
 import { compose, withState, withHandlers } from 'recompose'
 import { Legend, Section } from '../styles'
 
-const ValidationMenu = input => (
-  <div>
-    <Menu
-      {...input}
-      selectElement={value => {
-        input.onSelectElement(value)
-      }}
-    />
-    {input.selectelement && input.selectelement !== 'required' && (
-      <Section>
-        <Legend space>FIeld Min / Max</Legend>
-        <ValidatedFieldFormik
-          component={TextField}
-          name={`validateValue.${input.selectelement}`}
-        />
-      </Section>
-    )}
-  </div>
-)
+const ValidationMenu = input => {
+  const [validations, setValidations] = useState()
+
+  return (
+    <div>
+      <Select
+        {...input}
+        onChange={value => setValidations(value)}
+        selectElement={value => {
+          input.onSelectElement(value)
+        }}
+      />
+      {
+        // the actual value is an object with:
+        // [{"value":"minSize","label":"minSize"},{"value":"minChars","label":"minimum Characters"},{"value":"maxChars","label":"maximum Characters"}]"
+      }
+      {validations && validations !== 'required' && (
+        <Section>
+          <Legend space>Field Min / Max</Legend>
+          <ValidatedFieldFormik
+            component={TextField}
+            name={`validateValue.${input.selectelement}`}
+          />
+        </Section>
+      )}
+    </div>
+  )
+}
 
 export default compose(
   withState('selectelement', 'changeSelect', undefined),
