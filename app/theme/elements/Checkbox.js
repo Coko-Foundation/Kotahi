884aa4f6510e6377b59cbebe7f7d7003027733ac
Jure Triglav <juretriglav@gmail.com>
2020-06-09 16:12:10 +0200

    feat(theme): simplify checkbox to use native input elements

--- app/theme/elements/Checkbox.js
+++ app/theme/elements/Checkbox.js
@@ -20,41 +20,9 @@ const localBorderSize = '3px'
 const localBorderTwoSize = '1px'
 
 export default {
-  Label: css`
-    font-size: 1.1em;
-    font-style: italic;
-    letter-spacing: 1px;
-    transition: color 0.5s;
-
-    &:before {
-      content: ' ';
-      display: inline-block;
-      vertical-align: middle;
-
-      width: 0.7em;
-      height: 0.7em;
-      margin-right: 0.5em;
-
-      background: ${props => (props.checked ? 'currentColor' : 'transparent')};
-      border: ${localBorderSize} solid white;
-      box-shadow: 0 0 0 ${localBorderTwoSize} currentColor;
-
-      transition: border 0.5s ease, background-size 0.3s ease;
-    }
-  `,
-  Input: css`
-    position: absolute;
-    opacity: 0;
-    z-index: -1;
-
-    &:focus + span:before {
-      box-shadow: 0 0 ${th('borderWidth')} calc(${th('borderWidth')} * 2)
-        ${th('colorPrimary')};
-    }
-  `,
+  Label: css``,
+  Input: css``,
   Root: css`
-    transition: all 2s;
-
     &:hover span {
       color: ${th('colorPrimary')};
 
