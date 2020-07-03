1ff10100804067a277024963063fbb62c4bd1838
Jure Triglav <juretriglav@gmail.com>
2020-07-04 01:01:22 +0200

    feat: add standalone new submission page

--- app/routes.js
+++ app/routes.js
@@ -4,14 +4,15 @@ import { Route, Switch, Redirect } from 'react-router-dom'
 import Login from './components/component-login'
 import UsersManager from './components/component-users-manager'
 import Manuscripts from './components/component-manuscripts'
-import Dashboard from './components/component-xpub-dashboard/src/components/Dashboard'
-import SubmitPage from './components/component-xpub-submit/src/components/SubmitPage'
-import ManuscriptPage from './components/component-xpub-manuscript/src/components/ManuscriptPage'
-import ReviewersPage from './components/component-xpub-review/src/components/ReviewersPage'
-import ReviewPage from './components/component-xpub-review/src/components/ReviewPage'
-import TeamPage from './components/component-xpub-teams-manager/src/components/TeamsManagerPage'
-import DecisionPage from './components/component-xpub-review/src/components/DecisionPage'
-import FormBuilderPage from './components/component-xpub-formbuilder/src/components/FormBuilderPage'
+import Dashboard from './components/component-dashboard/src/components/Dashboard'
+import SubmitPage from './components/component-submit/src/components/SubmitPage'
+import ManuscriptPage from './components/component-manuscript/src/components/ManuscriptPage'
+import ReviewersPage from './components/component-review/src/components/ReviewersPage'
+import ReviewPage from './components/component-review/src/components/ReviewPage'
+import TeamPage from './components/component-teams-manager/src/components/TeamsManagerPage'
+import DecisionPage from './components/component-review/src/components/DecisionPage'
+import FormBuilderPage from './components/component-formbuilder/src/components/FormBuilderPage'
+import NewSubmissionPage from './components/component-submit/src/components/NewSubmissionPage'
 
 import { Profile } from './components/component-profile/src'
 
@@ -40,6 +41,11 @@ export default (
   <MainPage>
     <Switch>
       <PrivateRoute component={Dashboard} exact path="/journal/home" />
+      <PrivateRoute
+        component={NewSubmissionPage}
+        exact
+        path="/journal/newSubmission"
+      />
       <PrivateRoute
         component={SubmitPage}
         exact
