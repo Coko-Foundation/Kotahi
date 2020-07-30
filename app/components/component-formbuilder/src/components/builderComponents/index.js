bd50e544b3bcfc9b447911a0f31623b83bca5d24
Jure Triglav <juretriglav@gmail.com>
2020-07-30 13:30:31 +0200

    fix(formbuilder): quick fix for broken detail view

--- app/components/component-formbuilder/src/components/builderComponents/index.js
+++ app/components/component-formbuilder/src/components/builderComponents/index.js
@@ -5,6 +5,7 @@ import * as AbstractField from './AbstractField'
 import * as CheckBox from './CheckBox'
 import * as RadioBox from './RadioBox'
 import * as TextArea from './TextArea'
+import { Select } from '../../../../shared'
 
 export {
   OptionsField,
@@ -14,4 +15,5 @@ export {
   CheckBox,
   RadioBox,
   TextArea,
+  Select,
 }
