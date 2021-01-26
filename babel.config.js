bfd3d5a07ca5b6adbde0e78278b98c2befde7eca
Yannis Barlas <yannisbarlas@gmail.com>
2021-01-26 23:02:15 +0200

    chore(ui): add storybook

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: [
     'babel-plugin-styled-components',
-    '@babel/plugin-proposal-class-properties',
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
   ],
 }
