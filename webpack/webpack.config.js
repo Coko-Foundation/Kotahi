acd8b501a48214b2acc8c675c740c7af860b50f2
Jure Triglav <juretriglav@gmail.com>
2021-03-10 23:51:54 +0100

    fix: load .envs before config

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,15 +1,14 @@
 const path = require('path')
 const fs = require('fs-extra')
-const config = require('config')
 const { pick } = require('lodash')
+// Loads environment variables from e.g. .env.development, same as server/app.js
+const dotenvPath = path.resolve(`.env.${process.env.NODE_ENV}`)
+require('dotenv').config({ path: dotenvPath })
+const config = require('config')
 
 const rules = require('./common-rules')
 
 const contentBase = path.resolve(__dirname, '..', '_build', 'assets')
-
-// Loads environment variables from e.g. .env.development, same as server/app.js
-const dotenvPath = path.resolve(`.env.${config.util.getEnv('NODE_ENV')}`)
-require('dotenv').config({ path: dotenvPath })
 
 // can't use node-config in webpack so save whitelisted client config into the build and alias it below
 const clientConfig = pick(config, config.publicKeys)
