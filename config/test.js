ca5729470fa2618f42e7892c437cec48d6ca7f0a
Jure Triglav <juretriglav@gmail.com>
2021-03-10 15:11:53 +0100

    test: run tests in test env

--- config/test.js
+++ config/test.js
@@ -1,32 +1,16 @@
-const { deferConfig } = require('config/defer')
-const logger = require('winston')
-
 module.exports = {
   'pubsweet-server': {
     db: {
-      database: 'test',
+      database: 'kotahitest',
+      user: 'kotahitest',
     },
     pool: { min: 0, max: 10, idleTimeoutMillis: 1000 },
-    ignoreTerminatedConnectionError: true,
-    logger,
     uploads: 'uploads',
     port: 3000,
     secret: 'secret-string',
-    baseUrl: deferConfig(
-      cfg => `http://localhost:${cfg['pubsweet-server'].port}`,
-    ),
   },
-  'password-reset': deferConfig(
-    cfg => `http://localhost:${cfg['pubsweet-server'].port}/password-reset`,
-  ),
   mailer: {
-    transport: {
-      sendmail: false,
-      port: 1025,
-      auth: {
-        user: 'user',
-        pass: 'pass',
-      },
-    },
+    from: 'simplej@example.com',
+    path: `${__dirname}/test-mailer`,
   },
 }
