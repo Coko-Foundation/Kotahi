be7ed4aa27d3f8f0d175f165a2065d3aa8c0d839
Jure Triglav <juretriglav@gmail.com>
2021-03-20 16:19:23 +0100

    test: refer to the correct form builder element

--- cypress/integration/formbuilder_spec.js
+++ cypress/integration/formbuilder_spec.js
@@ -1,21 +1,24 @@
-import { FormsPage } from "../page-object/forms-page"
-import { Menu } from "../page-object/page-component/menu"
+import { FormsPage } from '../page-object/forms-page'
+import { Menu } from '../page-object/page-component/menu'
 
 describe('Form builder', () => {
   it('views a form field', () => {
-     // task to restore the database as per the  dumps/initialState.sql
-    cy.task('restore', 'initialState');
+    // task to restore the database as per the  dumps/initialState.sql
+    cy.task('restore', 'initialState')
 
-    //login as admin
-    cy.fixture("role_names").then(name => {
+    // login as admin
+    cy.fixture('role_names').then(name => {
       cy.login(name.role.admin) // Senior editor
-    });
+    })
 
-    //enter the from page and assert the fileds
-    Menu.clickForms();
+    // enter the from page and assert the fileds
+    Menu.clickForms()
 
-    FormsPage.getFormTitleTab(0).should('contain', 'Research Object Submission Form');
-    FormsPage.clickFormOption(1);
-    FormsPage.getNameField().should('have.value', 'submission.name');
-  });
-});
+    FormsPage.getFormTitleTab(0).should(
+      'contain',
+      'Research Object Submission Form',
+    )
+    FormsPage.clickFormOption(2)
+    FormsPage.getNameField().should('have.value', 'submission.name')
+  })
+})
