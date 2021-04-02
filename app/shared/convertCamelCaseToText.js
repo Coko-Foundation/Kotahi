57797b8acb2287d7edc8b5bc27364f81c2222141
Mihail Gorceag <mgorceag@EN410053.local>
2021-04-02 11:26:24 +0300

    fix: fix crash if not string is passed to fn

--- app/shared/convertCamelCaseToText.js
+++ app/shared/convertCamelCaseToText.js
@@ -1,5 +1,6 @@
-export function convertCamelCaseToText (text) {
-    const result = text.replace( /([A-Z])/g, " $1" )
-    const finalResult = result.charAt(0) + result.slice(1)
-    return finalResult.toLowerCase()
-  }
+export function convertCamelCaseToText(text) {
+  if (typeof text !== 'string') return ''
+  const result = text.replace(/([A-Z])/g, ' $1')
+  const finalResult = result.charAt(0) + result.slice(1)
+  return finalResult.toLowerCase()
+}
