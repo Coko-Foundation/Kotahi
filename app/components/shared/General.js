3594e29a512fca239e31f5c903582fbf89b9e92e
Ion Riciu <Ion.Riciu@endava.com>
2021-04-15 16:41:46 +0300

    revert: styling for manuscripts table

--- app/components/shared/General.js
+++ app/components/shared/General.js
@@ -13,8 +13,6 @@ export const Content = styled.div`
   box-shadow: ${th('boxShadow')};
   background-color: ${th('colorBackground')};
   border-radius: ${th('borderRadius')};
-  margin-top: ${grid(2)};
-  overflow-x: scroll;
 `
 
 export const SectionContent = styled(Section)`
