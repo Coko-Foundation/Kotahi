4a03f8acec89483445723c435defb7f14c396312
Ben Whitmore <ben.whitmore0@gmail.com>
2021-04-18 19:43:24 +1200

    fix(form-builder): fix seedForms script

--- scripts/seedForms.js
+++ scripts/seedForms.js
@@ -4,4 +4,4 @@ const { Form } = require('@pubsweet/models')
 const submissionFormStructure = require('../app/storage/forms/submit.json')
 
 const submissionForm = { purpose: 'submit', structure: submissionFormStructure }
-Form.createForm(null, { form: submissionForm }) // TODO how do I do this?
+Form.query().insert(submissionForm)
