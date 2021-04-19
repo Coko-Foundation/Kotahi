ae1b559fbe76bb0b1adc333a27e6c78a7bd47082
Ben Whitmore <ben.whitmore0@gmail.com>
2021-04-20 07:35:42 +1200

    fix(form-builder): don't seed form if one already exists

--- scripts/seedForms.js
+++ scripts/seedForms.js
@@ -1,4 +1,5 @@
 #! usr/bin/env node
+/* eslint-disable no-console */
 
 const { Form } = require('@pubsweet/models')
 const submissionFormStructure = require('../app/storage/forms/submit.json')
@@ -6,7 +7,14 @@ const submissionFormStructure = require('../app/storage/forms/submit.json')
 const submissionForm = { purpose: 'submit', structure: submissionFormStructure }
 
 const seed = async () => {
-  await Form.query().insert(submissionForm)
+  const [{ count }] = await Form.query().count()
+
+  if (count > 0) {
+    console.log('  Form(s) already exist in database. Skipping.')
+  } else {
+    await Form.query().insert(submissionForm)
+    console.log('  Added submission form to database.')
+  }
 }
 
 seed()
