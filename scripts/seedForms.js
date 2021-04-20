1c449dfca7ed11fb38d7a7d174fa56cb02b4dead
Ben Whitmore <ben.whitmore0@gmail.com>
2021-04-20 19:01:34 +1200

    fix(form-builder): seed from appropriate form file

--- scripts/seedForms.js
+++ scripts/seedForms.js
@@ -1,10 +1,16 @@
 #! usr/bin/env node
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
 /* eslint-disable no-console */
 
 const { Form } = require('@pubsweet/models')
-const submissionFormStructure = require('../app/storage/forms/submit.json')
 
-const submissionForm = { purpose: 'submit', structure: submissionFormStructure }
+const formPaths = {
+  aperture: '../app/storage/forms-aperture/submit.json',
+  colab: '../app/storage/forms-colab/submit.json',
+  elife: '../app/storage/forms/submit.json',
+  ncrc: '../app/storage/forms-ncrc/submit.json',
+}
 
 const seed = async () => {
   const [{ count }] = await Form.query().count()
@@ -12,8 +18,24 @@ const seed = async () => {
   if (count > 0) {
     console.log('  Form(s) already exist in database. Skipping.')
   } else {
+    const formPath = formPaths[process.env.INSTANCE_NAME]
+
+    if (!formPath) {
+      console.log(
+        `  No form file known for '${process.env.INSTANCE_NAME}' instance type. No forms were added to the database.`,
+      )
+      return
+    }
+
+    const submissionFormStructure = require(formPath)
+
+    const submissionForm = {
+      purpose: 'submit',
+      structure: submissionFormStructure,
+    }
+
     await Form.query().insert(submissionForm)
-    console.log('  Added submission form to database.')
+    console.log(`  Added submission form ${formPath} to database.`)
   }
 }
 
