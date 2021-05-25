c17bdf4759d30f2c3b9238a5cf7c046136688480
Mihail Gorceag <mgorceag@EN410053.local>
2021-05-25 10:27:14 +0300

    fix: google spreadsheet private key problem

--- server/model-manuscript/src/graphql.js
+++ server/model-manuscript/src/graphql.js
@@ -413,7 +413,10 @@ const resolvers = {
 
           await doc.useServiceAccountAuth({
             client_email: process.env.GOOGLE_SPREADSHEET_CLIENT_EMAIL,
-            private_key: process.env.GOOGLE_SPREADSHEET_PRIVATE_KEY,
+            private_key: process.env.GOOGLE_SPREADSHEET_PRIVATE_KEY.replace(
+              /\\n/g,
+              '\n',
+            ),
           })
 
           await doc.loadInfo()
