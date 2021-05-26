fe61d34e31f20e975ef166fb280cbf41efdda91a
Ion Riciu <Ion.Riciu@endava.com>
2021-05-26 17:33:17 +0300

    fix: publishing action

--- server/model-manuscript/src/manuscript.js
+++ server/model-manuscript/src/manuscript.js
@@ -248,7 +248,7 @@ class Manuscript extends BaseModel {
         submission: {},
         submitterId: { type: ['string', 'null'], format: 'uuid' },
         published: { type: ['string', 'object', 'null'], format: 'date-time' },
-        hypothesisPublicationId: { type: ['string', 'null'] },
+        evaluationsHypothesisMap: {},
       },
     }
   }
