0e22380aa0e6eb7b77855b33a7da4b4cb1eb2cf3
Ilia Eriomenco <Ilia.Eriomenco@endava.com>
2021-06-14 12:16:28 +0300

    ci: add S3_BUCKET in .env for Minio testing

---
+++ minio.js
@@ -0,0 +1,24 @@
+const AWS = require('aws-sdk');
+const accesskeid = process.env.S3_ACCESS_KEY_ID;
+const secretaccesskey = process.env.S3_ACCESS_KEY_SECRET;
+const s3endpoint = process.env.S3_ENDPOINT;
+const s3bucket = process.env.S3_BUCKET;
+
+const s3 = new AWS.S3({
+    accessKeyId: accesskeid,
+    secretAccessKey: secretaccesskey,
+    endpoint: s3endpoint,
+    s3ForcePathStyle: true,
+    signatureVersion: 'v4'
+});
+
+// putObject operation.
+
+const params = { Bucket: s3bucket, Key: 'testobject3', Body: 'Hello from MinIO!!' };
+
+s3.putObject(params, function (err, data) {
+    if (err)
+        console.log(err)
+    else
+        console.log("Successfully uploaded data to ", s3bucket, "/testobject3");
+});
