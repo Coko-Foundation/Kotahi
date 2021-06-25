b185502ba9e63a83c35a8f6d20475537ae79891d
Nicoleta Ursu <nicoleta.ursu@endava.com>
2021-06-25 14:45:21 +0300

    fix: paging is not displayed correctly

--- app/components/shared/Pagination.js
+++ app/components/shared/Pagination.js
@@ -1,55 +1,18 @@
-import React, { useState } from 'react'
-import { Action } from '@pubsweet/ui'
-import styled, { css } from 'styled-components'
+import React from 'react'
+import ReactPagination from 'react-paginate'
+import styled from 'styled-components'
 import { th, grid } from '@pubsweet/ui-toolkit'
 
-import { Icon } from './Icon'
-import { zIndex } from '../../globals'
-
-const Page = styled.div`
-  line-height: ${grid(5)};
-  width: ${grid(5)};
-  border: 1px solid ${th('colorFurniture')};
-  text-align: center;
-  margin-left: -1px;
-  button {
-    cursor: pointer;
-    color: ${th('colorText')};
-    width: ${grid(5)};
-    line-height: ${grid(5)};
-    left: -1px;
+const PaginationInfo = styled.div`
+  strong {
+    font-weight: 500;
   }
-
-  ${props =>
-    props.active &&
-    css`
-      z-index: ${zIndex.form};
-
-      button {
-        &[disabled],
-        &[disabled]:hover {
-          opacity: 1;
-          color: ${th('colorPrimary')};
-
-          :before {
-            visibility: visible;
-            opacity: 1;
-          }
-        }
-        // color: ${th('colorPrimary')};
-
-        &:before:[disabled] {
-          visibility: visible;
-          opacity: 1;
-        }
-      }
-    `}
 `
 
 export const PaginationContainer = styled.div`
+  align-items: center;
   display: flex;
   justify-content: space-between;
-  align-items: center;
   padding: ${grid(2)} ${grid(3)};
 `
 
@@ -59,87 +22,29 @@ export const PaginationContainerShadowed = styled(PaginationContainer)`
   box-shadow: ${th('boxShadow')};
 `
 
-const PaginationInfo = styled.div`
-  strong {
-    font-weight: 500;
-  }
-`
+const Styles = styled.div`
+  .pagination {
+    align-items: center;
+    background-color: ${th('colorBackground')};
+    border-radius: ${th('borderRadius')};
+    box-shadow: ${th('boxShadow')};
+    display: flex;
+    justify-content: space-between;
 
-const Paginators = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-`
-
-const PageDots = styled(Page)``
-
-const PreviousButton = styled.div`
-  border: 1px solid ${th('colorFurniture')};
-
-  border-radius: ${th('borderRadius')} 0 0 ${th('borderRadius')};
-
-  svg {
-    stroke: ${th('colorFurniture')};
-  }
-
-  button {
-    padding: ${grid(1)};
-    line-height: ${grid(5)};
-    width: ${grid(5)};
-    cursor: pointer;
-    svg {
-      stroke: ${th('colorText')};
+    li {
+      border: 1px solid ${th('colorFurniture')};
+      cursor: pointer;
+      list-style-type: none;
+      margin-left: -1px;
+      padding: ${grid(1)} ${grid(2)};
+      text-align: center;
     }
   }
+
+  .active {
+    background-color: #eee;
+  }
 `
-
-const NextButton = styled(PreviousButton)`
-  margin-left: -1px;
-  border-radius: 0 ${th('borderRadius')} ${th('borderRadius')} 0;
-`
-
-const Previous = ({ page, setPage, switchPages, first }) => (
-  <>
-    <PreviousButton>
-      <Action disabled={page <= 1} onClick={() => setPage(1)}>
-        <Icon noPadding>chevrons_left</Icon>
-      </Action>
-      <Action disabled={first <= 1} onClick={() => switchPages(-10)}>
-        <Icon noPadding>chevron_left</Icon>
-      </Action>
-    </PreviousButton>
-  </>
-)
-
-const Next = ({ page, pages, switchPages, setPage, lastPages, firstPages }) => {
-  const lastPage = page >= pages.length
-
-  return (
-    <>
-      <NextButton>
-        <Action
-          disabled={firstPages.includes(lastPages[0] - 1)}
-          onClick={() => switchPages(10)}
-        >
-          <Icon noPadding>chevron_right</Icon>
-        </Action>
-        <Action disabled={lastPage} onClick={() => setPage(pages.length)}>
-          <Icon noPadding>chevrons_right</Icon>
-        </Action>
-      </NextButton>
-    </>
-  )
-}
-
-const PageNumber = ({ pageNumber, setPage, page }) => {
-  const active = page === pageNumber
-  return (
-    <Page active={active}>
-      <Action disabled={active} onClick={() => setPage(pageNumber)}>
-        {pageNumber}
-      </Action>
-    </Page>
-  )
-}
 
 export const Pagination = ({
   setPage,
@@ -157,62 +62,26 @@ export const Pagination = ({
   const firstResult = (page - 1) * limit + 1
   const lastResult = Math.min((page - 1) * limit + limit, totalCount)
 
-  const lastPages = pages.slice(pages.length - 3, pages.length)
-
-  const [first, setFirst] = useState(page - 1)
-  const [last, setLast] = useState(page + 9)
-  const [firstPages, setFirstPages] = useState(pages.slice(first, last))
-
-  const switchPages = n => {
-    setFirst(first + n)
-    setLast(last + n)
-    setFirstPages(pages.slice(first + n, last + n))
-  }
-
   return (
     <PaginationContainer>
       <PaginationInfo>
         Showing <strong>{firstResult}</strong> to <strong>{lastResult}</strong>{' '}
         of <strong>{totalCount}</strong> results
       </PaginationInfo>
-      <Paginators>
-        <Previous {...{ page, setPage, switchPages, first }} />
-        {pages.length > 20 ? (
-          <>
-            {firstPages.map(pageNumber => (
-              <PageNumber
-                {...{ pageNumber, setPage, page }}
-                key={pageNumber}
-                pageNumber={pageNumber}
-              />
-            ))}
-
-            {!firstPages.includes(lastPages[0]) ? (
-              <>
-                <PageDots>...</PageDots>
-                {lastPages.map(pageNumber => (
-                  <PageNumber
-                    {...{ pageNumber, setPage, page }}
-                    key={pageNumber}
-                    pageNumber={pageNumber}
-                  />
-                ))}
-              </>
-            ) : null}
-          </>
-        ) : (
-          pages.map(pageNumber => (
-            <PageNumber
-              {...{ pageNumber, setPage, page }}
-              key={pageNumber}
-              pageNumber={pageNumber}
-            />
-          ))
-        )}
-        <Next
-          {...{ pages, page, switchPages, setPage, lastPages, firstPages }}
+      <Styles>
+        <ReactPagination
+          activeClassName="active"
+          containerClassName="pagination"
+          disableInitialCallback
+          initialPage={page - 1}
+          marginPagesDisplay={3}
+          nextLabel=">"
+          onPageChange={nmb => setPage(nmb.selected + 1)}
+          pageCount={pages.length}
+          pageRangeDisplayed={9}
+          previousLabel="<"
         />
-      </Paginators>
+      </Styles>
     </PaginationContainer>
   )
 }
