a3621ad074500345e4da2c5bb8c253ac4cf18c2b
Ben Whitmore <ben.whitmore0@gmail.com>
2021-07-01 20:35:43 +1200

    fix(reports): use react date picker, not input

--- app/components/component-reporting/src/DateRangePicker.js
+++ app/components/component-reporting/src/DateRangePicker.js
@@ -1,22 +1,14 @@
 import React from 'react'
-import styled, { css } from 'styled-components'
+import DatePicker from 'react-date-picker'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import { th, lighten } from '@pubsweet/ui-toolkit'
-import { getStartOfDayUtc, getEndOfDayUtc, getDateUtcString } from './dateUtils'
+import { getStartOfDayUtc, getEndOfDayUtc } from './dateUtils'
 
 const InlineBlock = styled.div`
   display: inline-block;
 `
 
-const Input = styled.input`
-  border: 1px solid
-    ${props => (props.isInvalid ? th('colorError') : th('colorBorder'))};
-  ${props =>
-    props.isInvalid &&
-    css`
-      background-color: ${lighten('colorError', 0.4)};
-    `}
-`
+const minDate = new Date('2020-01-01')
 
 // max specifies the latest date allowed, but as the date-range returned extends until
 // 23:59:59 UTC at the end of the endDate, it can overshoot max by some hours
@@ -24,34 +16,31 @@ const DateRangePicker = ({ endDate, max, setDateRange, startDate }) => {
   const trueMax = getEndOfDayUtc(max)
   const trueStart = getStartOfDayUtc(startDate)
   const trueEnd = getEndOfDayUtc(endDate)
-  const endIsInvalid = trueEnd > trueMax
-  const startIsInvalid = trueStart > trueMax || trueStart > trueEnd
   return (
     <InlineBlock>
-      <Input
-        isInvalid={startIsInvalid}
-        max={getDateUtcString(trueEnd)}
-        onChange={e =>
-          setDateRange(
-            getStartOfDayUtc(e.target.value).getTime(),
-            trueEnd.getTime(),
-          )
+      <DatePicker
+        clearIcon={null}
+        format="yyyy-MM-dd"
+        maxDate={trueEnd}
+        minDate={minDate}
+        onChange={val =>
+          setDateRange(getStartOfDayUtc(val).getTime(), trueEnd.getTime())
         }
-        type="date"
-        value={getDateUtcString(trueStart)}
+        value={trueStart}
       />
       {' — '}
-      <Input
-        isInvalid={endIsInvalid}
-        max={getDateUtcString(trueMax)}
-        onChange={e => {
+      <DatePicker
+        clearIcon={null}
+        format="yyyy-MM-dd"
+        maxDate={trueMax}
+        minDate={minDate}
+        onChange={val => {
           let newStart = trueStart.getTime()
-          const newEnd = getEndOfDayUtc(e.target.value).getTime()
+          const newEnd = getEndOfDayUtc(val).getTime()
           if (newStart >= newEnd) newStart = getStartOfDayUtc(newEnd).getTime()
           setDateRange(newStart, newEnd)
         }}
-        type="date"
-        value={getDateUtcString(trueEnd)}
+        value={trueEnd}
       />
     </InlineBlock>
   )
