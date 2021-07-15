f26fc8d825b45805859d6532f9be58a339d90afc
sebastianrad <65756657+sebastianrad@users.noreply.github.com>
2021-07-15 16:21:50 +0300

    test: add tests for admin user can see the review icons

--- cypress/integration/colab/control_page_spec.js
+++ cypress/integration/colab/control_page_spec.js
@@ -23,7 +23,7 @@ describe('review page', () => {
                 NewSubmissionPage.clickSubmitUrlAndWaitPageLoad()
                 Menu.clickManuscriptsAndAssertPageLoad()
                 ManuscriptsPage.clickControAndVerifyPageLoaded()
-                ControlPage.getAssignEditor(0).click()
+                ControlPage.getAssignEditor(3).click()
                 ControlPage.selectEditorByName(name.role.reviewers.reviewer1)
                 ControlPage.clickManageReviewers()
                 ReviewersPage.inviteReviewer(name.role.reviewers.reviewer1)
@@ -130,70 +130,128 @@ describe('review page', () => {
             ControlPage.getAcceptedToPublishReview().should('be.visible')
         })
     })
-})
-context('hide review and review name from author', () => {
-    beforeEach(() => {
-        cy.task('restore', 'initialState')
-        cy.task('seedForms')
-        // eslint-disable-next-line jest/valid-expect-in-promise
-        cy.fixture('role_names').then(name => {
-            cy.login(name.role.admin, dashboard)
-            cy.awaitDisappearSpinner()
-            DashboardPage.clickSubmit()
-            NewSubmissionPage.clickSubmitUrlAndWaitPageLoad()
-            Menu.clickManuscriptsAndAssertPageLoad()
-            ManuscriptsPage.clickControAndVerifyPageLoaded()
-            ControlPage.getAssignEditor(0).click()
-            ControlPage.selectEditorByName(name.role.reviewers.reviewer1)
-            ControlPage.clickManageReviewers()
-            ReviewersPage.inviteReviewer(name.role.reviewers.reviewer1)
-            ReviewersPage.clickSharedCheckbox(0)
-            ReviewersPage.waitThreeSec()
-            cy.login(name.role.reviewers.reviewer1, dashboard)
-            cy.awaitDisappearSpinner()
-            DashboardPage.clickAcceptReview()
-            DashboardPage.clickDoReview()
-            cy.fixture('submission_form_data').then(data => {
-                ReviewPage.fillInReviewComment(data.review1)
+
+    context('hide review and review name from author', () => {
+        beforeEach(() => {
+            cy.task('restore', 'initialState')
+            cy.task('seedForms')
+            // eslint-disable-next-line jest/valid-expect-in-promise
+            cy.fixture('role_names').then(name => {
+                cy.login(name.role.admin, dashboard)
+                cy.awaitDisappearSpinner()
+                DashboardPage.clickSubmit()
+                NewSubmissionPage.clickSubmitUrlAndWaitPageLoad()
+                Menu.clickManuscriptsAndAssertPageLoad()
+                ManuscriptsPage.clickControAndVerifyPageLoaded()
+                ControlPage.getAssignEditor(3).click()
+                ControlPage.selectEditorByName(name.role.reviewers.reviewer1)
+                ControlPage.clickManageReviewers()
+                ReviewersPage.inviteReviewer(name.role.reviewers.reviewer1)
+                ReviewersPage.clickSharedCheckbox(0)
+                ReviewersPage.waitThreeSec()
+                cy.login(name.role.reviewers.reviewer1, dashboard)
+                cy.awaitDisappearSpinner()
+                DashboardPage.clickAcceptReview()
+                DashboardPage.clickDoReview()
+                cy.fixture('submission_form_data').then(data => {
+                    ReviewPage.fillInReviewComment(data.review1)
+                })
+                ReviewPage.clickAccept()
+                ReviewPage.clickSubmit()
+                ReviewPage.waitThreeSec()
+                cy.login(name.role.admin, manuscripts)
+                cy.awaitDisappearSpinner()
+                ManuscriptsPage.clickControAndVerifyPageLoaded()
+                ControlPage.clickShow()
+                cy.fixture('submission_form_data').then(data => {
+                    ControlPage.getReviewMessage().should('contain', data.review1)
+                })
+                cy.login(name.role.reviewers.reviewer1, dashboard)
             })
-            ReviewPage.clickAccept()
-            ReviewPage.clickSubmit()
-            ReviewPage.waitThreeSec()
-            cy.login(name.role.admin, manuscripts)
-            cy.awaitDisappearSpinner()
-            ManuscriptsPage.clickControAndVerifyPageLoaded()
-            ControlPage.clickShow()
-            cy.fixture('submission_form_data').then(data => {
-                ControlPage.getReviewMessage().should('contain', data.review1)
-            })
-            cy.login(name.role.reviewers.reviewer1, dashboard)
-        })
             cy.awaitDisappearSpinner()
             DashboardPage.clickControlPanel()
-    })
-    it('review is hidden from the author of the article', () => {
-        ControlPage.clickHideReviewToAuthor()
-        // eslint-disable-next-line jest/valid-expect-in-promise
-        cy.fixture('role_names').then(name => {
-            cy.login(name.role.admin, manuscripts)
-            cy.awaitDisappearSpinner()
-            ManuscriptsPage.clickControl()
-            ControlPage.getNoReviewsMessage().should(
-                'contain',
-                'No reviews completed yet.',
-            )
-            ControlPage.getShowButton().should('not.exist')
+        })
+        it('review is hidden from the author of the article', () => {
+            ControlPage.clickHideReviewToAuthor()
+            // eslint-disable-next-line jest/valid-expect-in-promise
+            cy.fixture('role_names').then(name => {
+                cy.login(name.role.admin, manuscripts)
+                cy.awaitDisappearSpinner()
+                ManuscriptsPage.clickControl()
+                ControlPage.getNoReviewsMessage().should(
+                    'contain',
+                    'No reviews completed yet.',
+                )
+                ControlPage.getShowButton().should('not.exist')
+            })
+        })
+        it('hide reviewer name', () => {
+            ControlPage.clickHideReviewerNameToAuthor()
+            // eslint-disable-next-line jest/valid-expect-in-promise
+            cy.fixture('role_names').then(name => {
+                cy.login(name.role.admin, manuscripts)
+                cy.awaitDisappearSpinner()
+                ManuscriptsPage.clickControAndVerifyPageLoaded()
+                ControlPage.clickShow()
+                ControlPage.getReviewerName().should('contain', 'Anonymous')
+            })
+
         })
     })
-    it('hide reviewer name', () => {
-        ControlPage.clickHideReviewerNameToAuthor()
-        // eslint-disable-next-line jest/valid-expect-in-promise
-        cy.fixture('role_names').then(name => {
-            cy.login(name.role.admin, manuscripts)
-            cy.awaitDisappearSpinner()
-            ManuscriptsPage.clickControAndVerifyPageLoaded()
-            ControlPage.clickShow()
+
+    context.only('admin user can see the icons', () => {
+        beforeEach(() => {
+            cy.task('restore', 'initialState')
+            cy.task('seedForms')
+            // eslint-disable-next-line jest/valid-expect-in-promise
+            cy.fixture('role_names').then(name => {
+                cy.login(name.role.reviewers.reviewer1, dashboard)
+                cy.awaitDisappearSpinner()
+                DashboardPage.clickSubmit()
+                NewSubmissionPage.clickSubmitUrlAndWaitPageLoad()
+                cy.login(name.role.admin, manuscripts)
+                cy.awaitDisappearSpinner()
+                Menu.clickManuscriptsAndAssertPageLoad()
+                ManuscriptsPage.clickControAndVerifyPageLoaded()
+                ControlPage.getAssignEditor(3).click()
+                ControlPage.selectEditorByName(name.role.reviewers.reviewer2)
+                ControlPage.clickManageReviewers()
+                ReviewersPage.inviteReviewer(name.role.reviewers.reviewer2)
+                ReviewersPage.clickSharedCheckbox(0)
+                ReviewersPage.waitThreeSec()
+                cy.login(name.role.reviewers.reviewer2, dashboard)
+                cy.awaitDisappearSpinner()
+                DashboardPage.clickAcceptReview()
+                DashboardPage.clickDoReview()
+                cy.fixture('submission_form_data').then(data => {
+                    ReviewPage.fillInReviewComment(data.review1)
+                })
+                ReviewPage.clickCanBePublishedPublicly()
+                ReviewPage.clickAccept()
+                ReviewPage.clickSubmit()
+                ReviewPage.waitThreeSec()
+                cy.login(name.role.admin, manuscripts)
+                cy.awaitDisappearSpinner()
+                ManuscriptsPage.clickControAndVerifyPageLoaded()
+                ControlPage.clickShow()
+                cy.fixture('submission_form_data').then(data => {
+                    ControlPage.getReviewMessage().should('contain', data.review1)
+                })
+            })
+        })
+        it('admin user is able to see share icon, Hide review and Hide reviewer name checkboxs', () => {
+            ControlPage.getAcceptedToPublishReview().should('be.visible')
+            ControlPage.getHideReviewToAuthorCheckbox().should('be.visible')
+            ControlPage.getHideReviewerNameCheckbox().should('be.visible')
+        })
+        it('admin user hide reviewer name', () => {
+            ControlPage.clickHideReviewerNameToAuthor()
             ControlPage.getReviewerName().should('contain', 'Anonymous')
+            ControlPage.getHideReviewerNameCheckbox().should('be.visible')
+        })
+        it('admin user hide review', () => {
+            ControlPage.clickHideReviewToAuthor()
+            ControlPage.getHideReviewToAuthorCheckbox().should('be.visible')
         })
     })
 })
