987324a8a3a5674f43651e9af0fa746b953b16b2
Mihail Gorceag <mgorceag@EN410053.local>
2021-07-23 11:16:41 +0300

    fix: increased pool size for production

--- config/production.js
+++ config/production.js
@@ -1,1 +1,16 @@
-module.exports = {}
+module.exports = {
+  'pubsweet-server': {
+    db: {},
+    port: 3000,
+    pool: {
+      min: 0,
+      max: 1000,
+      createTimeoutMillis: 3000,
+      acquireTimeoutMillis: 30000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 100,
+      propagateCreateError: false,
+    },
+  },
+}
