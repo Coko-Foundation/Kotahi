f31e2cc7b1bff9df7b685bd6f574dfdf09bcde5d
Ben Whitmore <ben.whitmore0@gmail.com>
2021-08-16 16:54:49 +1200

    feat(dashboard): show shortId in dashboard if so configured

--- app/components/component-dashboard/src/components/sections/VersionTitle.js
+++ app/components/component-dashboard/src/components/sections/VersionTitle.js
@@ -4,16 +4,36 @@ import React from 'react'
 import styled from 'styled-components'
 import { th } from '@pubsweet/ui-toolkit'
 import { get } from 'lodash'
+import config from 'config'
+import lightenBy from '../../../../../shared/lightenBy'
 
 const Root = styled.div`
   line-height: ${th('lineHeightHeading4')};
 `
 
+const ShortId = styled.div`
+  color: ${lightenBy('colorText', 0.3)};
+  display: inline-block;
+  font-size: ${th('fontSizeBaseSmall')};
+  margin-right: 1em;
+  min-width: 3em;
+`
+
 export default ({ version, className }) => {
+  const shouldShowShortId =
+    config['client-features'].displayShortIdAsIdentifier &&
+    config['client-features'].displayShortIdAsIdentifier.toLowerCase() ===
+      'true'
+
   const title =
     process.env.INSTANCE_NAME === 'ncrc'
       ? JSON.parse(version.submission).articleDescription
       : get(version, 'meta.title') || 'Untitled'
 
-  return <Root>{title}</Root>
+  return (
+    <Root>
+      {shouldShowShortId && <ShortId>{version.shortId}</ShortId>}
+      {title}
+    </Root>
+  )
 }
