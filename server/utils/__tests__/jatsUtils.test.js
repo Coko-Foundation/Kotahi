77ff8139ab26be8a4f92b960558430d69fcd3c23
Ben Whitmore <ben.whitmore0@gmail.com>
2021-11-11 18:30:19 +1300

    feat(crossref): strip nonstandard links from jats

---
+++ server/utils/__tests__/jatsUtils.test.js
@@ -0,0 +1,47 @@
+const { htmlToJats } = require('../jatsUtils')
+
+describe('htmlToJats', () => {
+  test('illegalChars', () => {
+    expect(htmlToJats('ABC😉\x00\r\n\x0B\x9FXYZ')).toEqual('ABC😉\r\nXYZ')
+  })
+  test('insertSections', () => {
+    expect(
+      htmlToJats(`
+<h1>H1</h1>
+<h2>H2</h2>
+<h3>H3</h3>
+<h5>H5</h5>
+<h4>H4</h4>
+<h3>H3</h3>
+<h4>H4</h4>
+<h2>H2</h2>`),
+    ).toEqual(`
+<sec><title>H1</title>
+<sec><title>H2</title>
+<sec><title>H3</title>
+<sec><title>H5</title>
+</sec><sec><title>H4</title>
+</sec></sec><sec><title>H3</title>
+<sec><title>H4</title>
+</sec></sec></sec><sec><title>H2</title></sec></sec>`)
+  })
+  test('convertLinks', () => {
+    expect(
+      htmlToJats(`
+<a href="https://a.com" foo="bar">A</a>
+<a href="http://b.com">B</a>
+<a href="ftp://c.com:8080/123?x=y&z=w#ref1">C</a>
+<a href="#ref1">D</a>
+<a href="mailto:x@y.com">E</a>
+<a href="" download="resource.png">F</a>
+`),
+    ).toEqual(`
+<ext-link ext-link-type="uri" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="https://a.com">A</ext-link>
+<ext-link ext-link-type="uri" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="http://b.com">B</ext-link>
+<ext-link ext-link-type="uri" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="ftp://c.com:8080/123?x=y&z=w#ref1">C</ext-link>
+D
+E
+F
+`)
+  })
+})
