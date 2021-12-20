55aeae74e162aafc51fc7cbdc5c4da1b2ff58c6e
Dan Visel <dbvisel@gmail.com>
2021-12-20 20:08:08 +0800

    docs(ui): documented what it was doing

--- app/components/component-production/src/components/makePdf.js
+++ app/components/component-production/src/components/makePdf.js
@@ -13,7 +13,6 @@ const bearerToken =
 const pagedJsAPI = 'https://pagedjstest.cloud68.co/api/htmlToPDF'
 
 const makeZip = async htmlText => {
-  // get the ZIP stream in a Blob
   const blob = await downloadZip([
     {
       name: 'index.html',
@@ -21,19 +20,28 @@ const makeZip = async htmlText => {
       input: htmlText,
     },
     { name: 'styles.css', lastModified: new Date(), input: css },
+    // could insert an override CSS here by appending to styles.css
   ]).blob()
 
   return blob
 }
 
 const makePdf = async (html, articleMetadata) => {
+  // 0 strip content out of the HTML that shouldn't be in there (annotations)
   // 1 make HTML from html + front matter + CSS
+
   // console.log('Making PDF . . .')
+
+  // TODO: maybe put up a modal explaining that a PDF is being made?
+
+  // this runs the HTML from Wax through a template, feeding it variables
   const outHtml = makeTemplate(html, articleMetadata.title || '')
 
   // 2 zip this.
 
   const zipBlob = await makeZip(outHtml)
+
+  // this code is for testing the zip that's been created:
 
   // const url = URL.createObjectURL(zipBlob)
   // const link = document.createElement('a')
@@ -41,12 +49,12 @@ const makePdf = async (html, articleMetadata) => {
   // link.download = `${articleMetadata.title || 'title'}.zip`
   // link.click()
 
-  // console.log(zipUrl)
-
   // 3 check if we have an access token to the pagedjs server
 
-  // send to server
+  // 4 send to server
 
+  // eslint-disable-next-line no-console
+  console.log('Sending PDF to server!')
   const formData = new FormData()
   formData.append('zip', zipBlob, 'index.html.zip')
   axios
@@ -59,20 +67,30 @@ const makePdf = async (html, articleMetadata) => {
     })
     .then(async res => res.data)
     .then(resObj => {
+      // eslint-disable-next-line no-console
+      console.log('Retrieved PDF!')
       const newBlob = new Blob([resObj], { type: 'application/pdf' })
 
       const objUrl = window.URL.createObjectURL(newBlob)
+
+      // use this code to open PDF in new window:
+
+      window.open(objUrl)
+
+      // use this code for downloading the PDF:
 
       const link = document.createElement('a')
       link.href = objUrl
       link.download = `${articleMetadata.title || 'title'}.pdf`
       link.click()
+
       // console.log(`Downloading ${link.download}`)
 
       // For Firefox it is necessary to delay revoking the ObjectURL.
+
       setTimeout(() => {
         window.URL.revokeObjectURL(objUrl)
-      }, 250)
+      }, 1000)
     })
 
     .catch(err => console.error(err))
