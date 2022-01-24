082b31f29ebc505c66120b4d373b89ac71eeb093
Dan Visel <dbvisel@gmail.com>
2022-01-24 12:42:31 +0800

    fix(*): better downloading code

--- app/components/component-production/src/components/ProductionPage.js
+++ app/components/component-production/src/components/ProductionPage.js
@@ -68,19 +68,36 @@ const ProductionPage = ({ match, ...props }) => {
     })
   }
 
-  const makePdf = async () => {
-    console.log('in makePDF')
-
+  const makePdf = async title => {
+    // QUESTION: does this need to be a mostly invisible react component inside of the page wrapper?
     const { data, loading, error } = useQuery(getPdfQuery, {
       variables: {
         article: manuscript,
       },
     })
 
+    // TODO: deal with error handling in a smarter way
     if (loading) return 'loading'
     if (error) return 'error'
-    console.log(data)
-    // TODO: This needs to download the file!
+    // Now, download the file
+    // TODO: question: does this need to be turned into a blob?
+
+    window.open(data)
+
+    // use this code for downloading the PDF:
+
+    const link = document.createElement('a')
+    link.href = data
+    link.download = `${title || 'title'}.pdf`
+    link.click()
+
+    // console.log(`Downloading ${link.download}`)
+
+    // For Firefox it is necessary to delay revoking the ObjectURL.
+
+    setTimeout(() => {
+      window.URL.revokeObjectURL(data)
+    }, 1000)
     return data
   }
 
