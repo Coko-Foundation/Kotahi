d3122ae49be07a7d18f4f45b508ec8ebbed3288a
Dan Visel <dbvisel@gmail.com>
2022-01-26 13:03:07 +0800

    fix(ui): dumb syntax error fix

--- app/components/component-production/src/components/Production.js
+++ app/components/component-production/src/components/Production.js
@@ -26,63 +26,64 @@ const Production = ({
   manuscript,
   currentUser,
   makePdf,
-	downloadPdf,
+  downloadPdf,
   // fileUpload,
   updateManuscript,
 }) => {
-	if(downloadPdf) {
-		    // TODO: question: does this need to be turned into a blob?
-				window.open(downloadPdf)
+  if (downloadPdf) {
+    // TODO: question: does this need to be turned into a blob?
+    window.open(downloadPdf)
 
-				// use this code for downloading the PDF:
-		
-				const link = document.createElement('a')
-				link.href = downloadPdf
-				link.download = `${title || 'title'}.pdf`
-				link.click()
-		
-				// console.log(`Downloading ${link.download}`)
-		
-				// For Firefox it is necessary to delay revoking the ObjectURL.
-		
-				setTimeout(() => {
-					window.URL.revokeObjectURL(downloadPdf)
-					makePdf(false)
-				}, 1000)
-		
-	}
-	return (
-  <Container>
-    <HeadingWithAction>
-      <Heading>Production</Heading>
-      <DownloadDropdown
-        makePdf={makePdf}
-        metadata={manuscript}
-        source={manuscript.meta.source}
-      />
-    </HeadingWithAction>
-    {file &&
-    file.mimeType ===
-      'application/vnd.openxmlformats-officedocument.wordprocessingml.document' ? (
-      <SectionContent>
-        {manuscript ? (
-          <ProductionWaxEditor
-            onBlur={source => {
-              updateManuscript(manuscript.id, { meta: { source } })
-            }}
-            user={currentUser}
-            value={manuscript.meta.source}
-          />
-        ) : (
-          <Spinner />
-        )}
-      </SectionContent>
-    ) : (
-      <SectionContent>
-        <Info>No supported view of the file</Info>
-      </SectionContent>
-    )}
-  </Container>
-)
+    // use this code for downloading the PDF:
+
+    const link = document.createElement('a')
+    link.href = downloadPdf
+    link.download = `${title || 'title'}.pdf`
+    link.click()
+
+    // console.log(`Downloading ${link.download}`)
+
+    // For Firefox it is necessary to delay revoking the ObjectURL.
+
+    setTimeout(() => {
+      window.URL.revokeObjectURL(downloadPdf)
+      makePdf(false)
+    }, 1000)
+  }
+
+  return (
+    <Container>
+      <HeadingWithAction>
+        <Heading>Production</Heading>
+        <DownloadDropdown
+          makePdf={makePdf}
+          metadata={manuscript}
+          source={manuscript.meta.source}
+        />
+      </HeadingWithAction>
+      {file &&
+      file.mimeType ===
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document' ? (
+        <SectionContent>
+          {manuscript ? (
+            <ProductionWaxEditor
+              onBlur={source => {
+                updateManuscript(manuscript.id, { meta: { source } })
+              }}
+              user={currentUser}
+              value={manuscript.meta.source}
+            />
+          ) : (
+            <Spinner />
+          )}
+        </SectionContent>
+      ) : (
+        <SectionContent>
+          <Info>No supported view of the file</Info>
+        </SectionContent>
+      )}
+    </Container>
+  )
+}
 
 export default withRouter(Production)
