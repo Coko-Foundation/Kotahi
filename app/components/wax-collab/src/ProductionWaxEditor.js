797ca704efed6975673ea124d723f7d8b1da4eca
Dan Visel <dbvisel@gmail.com>
2022-06-23 15:36:21 +0800

    fix(api): fix for half of crash

--- app/components/wax-collab/src/ProductionWaxEditor.js
+++ app/components/wax-collab/src/ProductionWaxEditor.js
@@ -57,7 +57,7 @@ const ProductionWaxEditor = ({
 
   useEffect(() => {
     return () => {
-      if (editorRef.current) {
+      if (editorRef.current && saveSource !== null) {
         saveSource(editorRef.current.getContent())
       }
     }
