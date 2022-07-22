3db648d77ecdd433576037d2261d89bae0b4eadf
Snehil Raj <snehil.raj@coloredcow.in>
2022-07-22 16:20:54 +0530

    fix(test): added separate spec for assigning editor

--- cypress/integration/aperture/001-pdf_submission_spec.js
+++ cypress/integration/aperture/001-pdf_submission_spec.js
@@ -4,8 +4,6 @@
 import { DashboardPage } from '../../page-object/dashboard-page'
 import { SubmissionFormPage } from '../../page-object/submission-form-page'
 import { Menu } from '../../page-object/page-component/menu'
-import { ManuscriptsPage } from '../../page-object/manuscripts-page'
-import { ControlPage } from '../../page-object/control-page'
 import { dashboard } from '../../support/routes'
 
 describe('Upload manuscript test', () => {
@@ -51,47 +49,5 @@ describe('Upload manuscript test', () => {
 
       cy.task('dump', 'submission_complete')
     })
-  })
-
-  it('senior editor can view the submission', () => {
-    // task to restore the database as per the  dumps/submission_complete.sql
-    cy.task('restore', 'submission_complete')
-    cy.task('seedForms')
-
-    cy.fixture('submission_form_data').then(data => {
-      cy.fixture('role_names').then(name => {
-        // login as admin
-        cy.login(name.role.admin, dashboard)
-
-        // enter email
-        cy.contains('Enter Email').click()
-        cy.get('#enter-email').type('admin@gmail.com')
-
-        // submit the email
-        cy.contains('Next').click()
-
-        // select Control on the Manuscripts page
-        Menu.clickManuscripts()
-
-        ManuscriptsPage.selectOptionWithText('Control')
-
-        // assign seniorEditor
-        ControlPage.clickAssignSeniorEditorDropdown()
-        ControlPage.selectDropdownOptionByName(name.role.seniorEditor.uuid)
-        ControlPage.clickAssignHandlingEditorDropdown()
-        ControlPage.selectDropdownOptionByName(name.role.seniorEditor.uuid)
-        ControlPage.clickAssignEditorDropdown()
-        ControlPage.selectDropdownOptionByName(name.role.seniorEditor.uuid1)
-        // assert the reviews
-        ControlPage.fillInDecision(data.decision)
-        ControlPage.clickAccept()
-        ControlPage.clickSubmit()
-      })
-    })
-
-    // task to dump data in dumps/senior_editor_assigned.sql
-    cy.task('dump', 'senior_editor_assigned')
-
-    cy.contains('Dashboard').click()
   })
 })
