090d1673faf6345dcb5723e4c8baff5b9bc4ad55
Dan Visel <dbvisel@gmail.com>
2022-08-29 13:07:27 +0800

    fix(ui): footnote export fix

---
+++ server/jatsexport/makeCitations.js
@@ -0,0 +1,134 @@
+const { htmlToJats } = require('../utils/jatsUtils')
+
+const makeCitations = html => {
+  console.log('in makeCitations')
+  let deCitedHtml = html
+  let refList = '' // this is the ref-list that we're building
+  let refListHeader = '' // if there's a header, it goes in here
+  let refCount = 0 // this is to give refs IDs
+  const potentialRefs = []
+
+  while (deCitedHtml.indexOf('<section class="reflist">') > -1) {
+    let thisRefList = deCitedHtml
+      .split('<section class="reflist">')[1]
+      .split('</section>')[0]
+
+    deCitedHtml = deCitedHtml.replace(
+      `<section class="reflist">${thisRefList}</section>`,
+      '',
+    )
+
+    // 2.1. Get header, if there is one. Only the first reflist header is taken.
+    if (!refListHeader) {
+      if (thisRefList.indexOf('<h1 class="referenceheader">') > -1) {
+        /* eslint-disable prefer-destructuring */
+        refListHeader = thisRefList
+          .split('<h1 class="referenceheader">')[1]
+          .split('</h1>')[0]
+        refList = `<title>${refListHeader}</title>${refList}`
+      }
+    }
+    // 2.2. Get all the citations out, add to refList
+
+    // first, go through and identify all possible mixed citations
+
+    while (thisRefList.indexOf('<p class="mixedcitation">') > -1) {
+      const thisCitation = thisRefList
+        .split('<p class="mixedcitation">')[1]
+        .split('</p>')[0]
+
+      if (thisCitation.length) {
+        potentialRefs[
+          html.indexOf(`<p class="mixedcitation">${thisCitation}</p>`)
+        ] = thisCitation
+      }
+
+      thisRefList = thisRefList.replace(
+        `<p class="mixedcitation">${thisCitation}</p>`,
+        ``,
+      )
+    }
+
+    // next, take all regular paragraphs as citations
+
+    while (thisRefList.indexOf('<p class="paragraph">') > -1) {
+      const thisCitation = thisRefList
+        .split('<p class="paragraph">')[1]
+        .split('</p>')[0]
+
+      if (thisCitation.length) {
+        potentialRefs[
+          html.indexOf(`<p class="paragraph">${thisCitation}</p>`)
+        ] = thisCitation
+      }
+
+      thisRefList = thisRefList.replace(
+        `<p class="paragraph">${thisCitation}</p>`,
+        ``,
+      )
+    }
+
+    // finally, if there are <li>s with content, take them as citations.
+
+    while (thisRefList.indexOf('<li>') > -1) {
+      const thisCitation = thisRefList.split('<li>')[1].split('</li>')[0]
+
+      if (thisCitation.length) {
+        potentialRefs[html.indexOf(`<li>${thisCitation}</li>`)] = thisCitation
+      }
+
+      thisRefList = thisRefList.replace(`<li>${thisCitation}</li>`, ``)
+    }
+
+    const myRefs = potentialRefs.filter(x => x)
+
+    refList += myRefs
+      .map(
+        (thisCitation, index) =>
+          `<ref id="ref-${index}"><mixed-citation>${htmlToJats(
+            thisCitation,
+          )}</mixed-citation></ref>`,
+      )
+      .join('')
+    refCount = myRefs.length
+  }
+
+  // 2.3 deal with any stray reference headers in the body—they become regular H1s.
+
+  while (deCitedHtml.indexOf('<h1 class="referenceheader">') > -1) {
+    deCitedHtml = deCitedHtml.replace(`<h1 class="referenceheader">`, '<h1>')
+  }
+
+  // 2.4 deal with any loose mixed citations in the body:
+  // they're pulled out of the body and added to the ref-list
+  // QUESTION: Is this the right thing to do? It isn't necessarily what the user expects.
+  // Theoretically you could have a <ref-list> at the end of a <sec> though why you would want
+  // that is not clear to me. <mixed-citation> by itself isn't valid in a <sec> (even wrapped in <ref>).
+  // The alternative would just be to delete the loose <mixed-citations>?
+
+  while (deCitedHtml.indexOf('<p class="mixedcitation">') > -1) {
+    const thisCitation = deCitedHtml
+      .split('<p class="mixedcitation">')[1]
+      .split('</p>')[0]
+
+    deCitedHtml = deCitedHtml.replace(
+      `<p class="mixedcitation">${thisCitation}</p>`,
+      ``,
+    )
+
+    // TODO: get all the <span class="mixed-citation"> out of deCitedHtml
+
+    refList += `<ref id="ref-${refCount}"><mixed-citation>${htmlToJats(
+      thisCitation,
+    )}</mixed-citation></ref>`
+    refCount += 1
+  }
+
+  if (refList) {
+    refList = `<ref-list>${refList}</ref-list>`
+  }
+
+  return { deCitedHtml, refList }
+}
+
+module.exports = makeCitations
--- server/utils/jatsUtils.js
+++ server/utils/jatsUtils.js
@@ -1,6 +1,7 @@
 const he = require('he')
 const htmlparser2 = require('htmlparser2')
 const cheerio = require('cheerio')
+const makeCitations = require('../jatsexport/makeCitations')
 
 // const { lte } = require('semver')
 
@@ -224,6 +225,7 @@ const convertImages = html => {
 }
 
 const htmlToJats = html => {
+  // console.log('in html to jats', html)
   let jats = html
   jats = removeIllegalCharacters(jats)
   jats = insertSections(jats)
@@ -432,15 +434,17 @@ const makeFootnotesSection = html => {
 
   while (deFootnotedHtml.indexOf(`<footnote id="`) > -1) {
     // get id and text from Wax markup
-    const [id, text] = deFootnotedHtml
-      .split('<footnote id="')[1]
-      .split('</footnote')[0]
-      .split('">')
+    const id = deFootnotedHtml.split('<footnote id="')[1].split('">')[0]
 
+    const text = deFootnotedHtml
+      .split(`<footnote id="${id}">`)[1]
+      .split('</footnote>')[0]
+
+    const toReplace = `<footnote id="${id}">${text}</footnote>`
     footnoteCount += 1
     // replace body text with JATS tag
     deFootnotedHtml = deFootnotedHtml.replace(
-      `<footnote id="${id}">${text}</footnote>`,
+      toReplace,
       `<xref ref-type="fn" rid="fnid${id}">${footnoteCount}</xref>`,
     )
     // add this to the list of footnotes
@@ -558,133 +562,6 @@ const makeAppendices = html => {
       ? `<app-group>${appendices.join('')}</app-group>`
       : '',
   }
-}
-
-const makeCitations = html => {
-  let deCitedHtml = html
-  let refList = '' // this is the ref-list that we're building
-  let refListHeader = '' // if there's a header, it goes in here
-  let refCount = 0 // this is to give refs IDs
-  const potentialRefs = []
-
-  while (deCitedHtml.indexOf('<section class="reflist">') > -1) {
-    let thisRefList = deCitedHtml
-      .split('<section class="reflist">')[1]
-      .split('</section>')[0]
-
-    deCitedHtml = deCitedHtml.replace(
-      `<section class="reflist">${thisRefList}</section>`,
-      '',
-    )
-
-    // 2.1. Get header, if there is one. Only the first reflist header is taken.
-    if (!refListHeader) {
-      if (thisRefList.indexOf('<h1 class="referenceheader">') > -1) {
-        /* eslint-disable prefer-destructuring */
-        refListHeader = thisRefList
-          .split('<h1 class="referenceheader">')[1]
-          .split('</h1>')[0]
-        refList = `<title>${refListHeader}</title>${refList}`
-      }
-    }
-    // 2.2. Get all the citations out, add to refList
-
-    // first, go through and identify all possible mixed citations
-
-    while (thisRefList.indexOf('<p class="mixedcitation">') > -1) {
-      const thisCitation = thisRefList
-        .split('<p class="mixedcitation">')[1]
-        .split('</p>')[0]
-
-      if (thisCitation.length) {
-        potentialRefs[
-          html.indexOf(`<p class="mixedcitation">${thisCitation}</p>`)
-        ] = thisCitation
-      }
-
-      thisRefList = thisRefList.replace(
-        `<p class="mixedcitation">${thisCitation}</p>`,
-        ``,
-      )
-    }
-
-    // next, take all regular paragraphs as citations
-
-    while (thisRefList.indexOf('<p class="paragraph">') > -1) {
-      const thisCitation = thisRefList
-        .split('<p class="paragraph">')[1]
-        .split('</p>')[0]
-
-      if (thisCitation.length) {
-        potentialRefs[
-          html.indexOf(`<p class="paragraph">${thisCitation}</p>`)
-        ] = thisCitation
-      }
-
-      thisRefList = thisRefList.replace(
-        `<p class="paragraph">${thisCitation}</p>`,
-        ``,
-      )
-    }
-
-    // finally, if there are <li>s with content, take them as citations.
-
-    while (thisRefList.indexOf('<li>') > -1) {
-      const thisCitation = thisRefList.split('<li>')[1].split('</li>')[0]
-
-      if (thisCitation.length) {
-        potentialRefs[html.indexOf(`<li>${thisCitation}</li>`)] = thisCitation
-      }
-
-      thisRefList = thisRefList.replace(`<li>${thisCitation}</li>`, ``)
-    }
-
-    const myRefs = potentialRefs.filter(x => x)
-
-    refList += myRefs
-      .map(
-        (thisCitation, index) =>
-          `<ref id="ref-${index}"><mixed-citation>${htmlToJats(
-            thisCitation,
-          )}</mixed-citation></ref>`,
-      )
-      .join('')
-    refCount = myRefs.length
-  }
-
-  // 2.3 deal with any stray reference headers in the body—they become regular H1s.
-
-  while (deCitedHtml.indexOf('<h1 class="referenceheader">') > -1) {
-    deCitedHtml = deCitedHtml.replace(`<h1 class="referenceheader">`, '<h1>')
-  }
-
-  // 2.4 deal with any loose mixed citations in the body:
-  // they're pulled out of the body and added to the ref-list
-  // QUESTION: Is this the right thing to do? It isn't necessarily what the user expects.
-  // Theoretically you could have a <ref-list> at the end of a <sec> though why you would want
-  // that is not clear to me. <mixed-citation> by itself isn't valid in a <sec> (even wrapped in <ref>).
-  // The alternative would just be to delete the loose <mixed-citations>?
-
-  while (deCitedHtml.indexOf('<p class="mixedcitation">') > -1) {
-    const thisCitation = deCitedHtml
-      .split('<p class="mixedcitation">')[1]
-      .split('</p>')[0]
-
-    deCitedHtml = deCitedHtml.replace(
-      `<p class="mixedcitation">${thisCitation}</p>`,
-      ``,
-    )
-    refList += `<ref id="ref-${refCount}"><mixed-citation>${htmlToJats(
-      thisCitation,
-    )}</mixed-citation></ref>`
-    refCount += 1
-  }
-
-  if (refList) {
-    refList = `<ref-list>${refList}</ref-list>`
-  }
-
-  return { deCitedHtml, refList }
 }
 
 const makeFrontMatter = html => {
