0d56f2bfafe858127c6c2304030eb1ddd7400293
Snehil Raj <snehil.raj@coloredcow.in>
2022-09-10 16:56:16 +0530

    feat(ui): removed refresh button from coLab instance

--- app/components/component-manuscripts/src/Manuscripts.js
+++ app/components/component-manuscripts/src/Manuscripts.js
@@ -291,7 +291,7 @@ const Manuscripts = ({ history, ...props }) => {
               </FloatRightButton>
             )}
 
-            {['ncrc', 'colab'].includes(process.env.INSTANCE_NAME) && (
+            {['ncrc'].includes(process.env.INSTANCE_NAME) && (
               <FloatRightButton
                 disabled={isImporting}
                 onClick={importManuscripts}
