b268ad216c9cf8aadec92f047e8795f268dde486
Snehil Raj <snehil.raj@coloredcow.in>
2022-09-19 19:21:55 +0530

    feat(sub): code refactor

--- server/app.js
+++ server/app.js
@@ -1,18 +1,18 @@
-// server/app.js
-
-// This is the express app your app will use
 const { app } = require('@coko/server')
 
-// You can modify the app or ensure other things are imported here
+// You can modify the app or ensure other things are imported here or you can use this to define functions globally here.
 const schedule = require('../node_modules/node-schedule')
 
 const {
   importManuscripts,
 } = require('./model-manuscript/src/manuscriptCommsUtils')
 
-schedule.scheduleJob({ tz: 'Etc/UTC', rule: '00 21 * * *' }, () => {
-  // eslint-disable-next-line no-unused-expressions, no-sequences
-  importManuscripts({ user: null })
+schedule.scheduleJob({ tz: 'Etc/UTC', rule: '00 21 * * *' }, async () => {
+  try {
+    await importManuscripts({ user: null })
+  } catch (error) {
+    console.error(error)
+  }
 })
 
 module.exports = app
