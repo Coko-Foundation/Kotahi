aab3e7c943bd61932c4ac497584e48340a842b8d
Snehil Raj <snehil.raj@coloredcow.in>
2022-09-23 20:08:46 +0530

    feat(sub): removed unecessary migration

--- server/model-manuscript/src/migrations/1663072980-alter_manuscript_ishidden.sql
+++
@@ -1,4 +0,0 @@
-UPDATE manuscripts SET is_hidden = false where is_hidden is NULL;
-ALTER TABLE manuscripts ALTER is_hidden SET NOT NULL;
-
-ALTER TABLE manuscripts ALTER is_hidden SET DEFAULT false;
