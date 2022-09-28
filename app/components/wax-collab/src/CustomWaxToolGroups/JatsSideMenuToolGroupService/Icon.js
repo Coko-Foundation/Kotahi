8bc7babbec695088cae6b3b7587d6d989217c8b7
Dan Visel <dbvisel@gmail.com>
2022-09-28 13:01:21 +0800

    fix(ui): fix title on citation tab by moving icon locally

--- app/components/wax-collab/src/CustomWaxToolGroups/JatsSideMenuToolGroupService/Icon.js
+++ app/components/wax-collab/src/CustomWaxToolGroups/JatsSideMenuToolGroupService/Icon.js
@@ -19,11 +19,10 @@ const Svg = styled.svg.attrs(() => ({
 `
 
 const kotahiIcons = {
-  // TODO: insert new icons here
-  demoCommentBubble: ({ className }) => (
-    <Svg className={className} viewBox="0 0 24 24">
+  citationList: ({ className }) => (
+    <Svg className={className} fill="none" viewBox="0 0 24 24">
       <path d="M0 0h24v24H0z" fill="none" />
-      <path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zM6 9h12v2H6V9zm8 5H6v-2h8v2zm4-6H6V6h12v2z" />
+      <path d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z" />
     </Svg>
   ),
   backMatter: ({ className }) => (
