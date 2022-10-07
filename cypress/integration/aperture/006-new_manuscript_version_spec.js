c4079a0444d0e6cbbf32b1d9231c31fb135a6e9e
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2022-10-07 18:22:35 +0530

    updated code as per commemnts in MR-609

--- cypress/integration/aperture/006-new_manuscript_version_spec.js
+++ cypress/integration/aperture/006-new_manuscript_version_spec.js
@@ -13,47 +13,55 @@ describe('checking manuscript version', () => {
     cy.task('seed', 'three_reviews_completed') 
     cy.task('seedForms')
     cy.fixture('role_names').then(name => {
-      // login as admin
+      /* login as admin */
       cy.login(name.role.admin.name, dashboard)
       DashboardPage.clickManuscriptNavButton()
-      ManuscriptsPage.clickControlButton()
+      ManuscriptsPage.clickControlButton()  
+      /* Assign Editor */
       ControlPage.getAssignSeniorEditorDropdown()
         .click({ force: true })
-        .type(`${name.role.seniorEditor.username}{enter}`) // Assign Editor
+        .type(`${name.role.seniorEditor.username}{enter}`)  
 
-       // Edditor  Submits a decision 
+       /* Editor  Submits a decision */
        cy.login(name.role.seniorEditor.name, dashboard)
        DashboardPage.clickControlPanel()
-       ControlPage.getPublishButton().should('be.disabled') // Verify publish button is disabled
-       // Fill the decision form
+       /* Verify publish button is disabled */
+       ControlPage.getPublishButton().should('be.disabled')  
+       /* Fill the decision form */
        ControlPage.clickDecisionTextInput()
        ControlPage.getDecisionTextInput().type(decisionTextContent)
        ControlPage.getDecisionFileInput().attachFile(decisionFileName)
        ControlPage.clickRevise()
-       ControlPage.clickSubmitDecisionButton() // Submit the decision
-       ControlPage.checkSvgExists() // Check appears in front of button
+       /* Submit the decision */
+       ControlPage.clickSubmitDecisionButton()  
+       /* Check appears in front of button */
+       ControlPage.checkSvgExists()  
 
-      // View Decision as an Author 
-      cy.login(name.role.author.name, dashboard) // Login as an Author
-      DashboardPage.getSubmittedManuscript().click() // Click on first MySubmission
+      /* View Decision as an Author */
+      cy.login(name.role.author.name, dashboard) 
+      /* Click on first MySubmission */
+      DashboardPage.getSubmittedManuscript().click()  
       // Verify Decision Content
       DashboardPage.getDecisionField(0).should('contain', decisionTextContent)
       DashboardPage.getDecisionField(1).should('contain', decisionFileName)
       DashboardPage.getDecisionField(2).should('contain', 'revise')
-      DashboardPage.clickCreateNewVersionButton() // Create new manuscript version
+      /* Create new manuscript version */
+      DashboardPage.clickCreateNewVersionButton() 
       SubmissionFormPage.getTypeOfResearchObject()
         .click()
         .type('Dataset{enter}')
       SubmissionFormPage.clickSubmitResearch()
       SubmissionFormPage.clickSubmitYourManuscript()
+      /* Verify new submission got created */
       DashboardPage.getSubmittedManuscript()
         .contains('test pdf')
-        .should('exist') // Verify new submission got created
+        .should('exist') 
 
-      // Login as editor and check the new version submission form
+      /* Login as editor and check the new version submission form */
       cy.login(name.role.seniorEditor.name, dashboard)
       DashboardPage.clickControlPanel()
-      ControlPage.getPublishButton().should('be.disabled') // Verify publish button is disabled
+      /* Verify publish button is disabled */
+      ControlPage.getPublishButton().should('be.disabled') 
     })
   })
 })
