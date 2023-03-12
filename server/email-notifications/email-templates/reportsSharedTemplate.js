da6902efe84d4b52c61adc967f59b9b03c797cfe
Vaibhav Rathore <vaibhav@coloredcow.com>
2023-03-12 16:25:32 +0530

    reports shared changed submission link to manuscript url

--- server/email-notifications/email-templates/reportsSharedTemplate.js
+++ server/email-notifications/email-templates/reportsSharedTemplate.js
@@ -1,6 +1,6 @@
 const ReportsSharedTemplate = ({
   receiverName,
-  submissionLink,
+  manuscriptPageUrl,
 }) => {
   const result = {
     cc: '',
@@ -12,7 +12,7 @@ const ReportsSharedTemplate = ({
   result.subject = 'Report(s) for Biophysics Colab available to view'
   result.content = `<p>
       <p>Dear ${receiverName}</p>
-      <p>One or more reports for a preprint to which you are assigned are available to view at <a href="${submissionLink}">${submissionLink}</a>. Please log in to read these reports and provide your comments using the chat function.</p>
+      <p>One or more reports for a preprint to which you are assigned are available to view at <a href="${manuscriptPageUrl}">${manuscriptPageUrl}</a>. Please log in to read these reports and provide your comments using the chat function.</p>
       <p>Thank you</p>
       <p>
         On behalf of Biophysics Colab <br>
