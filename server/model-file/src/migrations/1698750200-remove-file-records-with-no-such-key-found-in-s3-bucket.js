e870985482a0dcab38ca5f6ff78c286cda96702d
Vignesh Devendran <vigneshdevendran7@gmail.com>
2023-12-21 04:14:14 +0000

    feat(file): upgrade coko server to v2.5.3 for main branch

---
+++ server/model-file/src/migrations/1698750200-remove-file-records-with-no-such-key-found-in-s3-bucket.js
@@ -0,0 +1,56 @@
+const fs = require('fs-extra')
+const path = require('path')
+const { fileStorage, File, logger, useTransaction } = require('@coko/server')
+
+const {
+  connectToFileStorage,
+} = require('@coko/server/src/services/fileStorage')
+
+exports.up = async knex => {
+  return useTransaction(async trx => {
+    await connectToFileStorage()
+    const files = await File.query(trx)
+
+    logger.info(`Total file records in table: ${files.length}`)
+
+    const tempDir = path.join(__dirname, '..', 'temp')
+    await fs.ensureDir(tempDir)
+
+    const filesWithNoSuchKeyFound = []
+
+    await Promise.all(
+      files.map(async file => {
+        const { key } = file.storedObjects.find(
+          storedObject => storedObject.type === 'original',
+        )
+
+        const tempPath = path.join(tempDir, `${file.id}_${key}`)
+
+        try {
+          await fileStorage.download(key, tempPath)
+        } catch (error) {
+          if (error.message === 'The specified key does not exist.') {
+            filesWithNoSuchKeyFound.push(file)
+          } else {
+            logger.info(error.message, key)
+            throw new Error(error)
+          }
+        }
+
+        fs.unlinkSync(tempPath)
+      }),
+    ).then(async res => {
+      logger.info(
+        `Number of file records to be removed: ${filesWithNoSuchKeyFound.length}`,
+      )
+
+      const affectedRows = await Promise.all(
+        filesWithNoSuchKeyFound.map(async f =>
+          File.query(trx).deleteById(f.id),
+        ),
+      )
+
+      logger.info(`Number of removed file records: ${affectedRows.length}`)
+    })
+  })
+}
