12f6d5591528aeae9365f89fceb0176213a4d658
Giannis Kopanas <jkopanas@gmail.com>
2024-09-13 09:23:17 +0300

    fix(ui): send files to external s3 service

--- packages/client/app/components/component-formbuilder/src/components/builderComponents/index.js
+++ packages/client/app/components/component-formbuilder/src/components/builderComponents/index.js
@@ -4,7 +4,6 @@ import TextFieldShowHide from './TextFieldShowHide'
 import AbstractField from './AbstractField'
 import CheckBox from './CheckBox'
 import RadioBox from './RadioBox'
-import RadioBoxS3Service from './RadioBoxS3Service'
 import TextArea from './TextArea'
 import FullWaxField from './FullWaxField'
 import ThreadedDiscussion from './ThreadedDiscussion/ThreadedDiscussion'
@@ -20,7 +19,6 @@ export {
   AbstractField,
   CheckBox,
   RadioBox,
-  RadioBoxS3Service,
   TextArea,
   ThreadedDiscussion,
   Select,
