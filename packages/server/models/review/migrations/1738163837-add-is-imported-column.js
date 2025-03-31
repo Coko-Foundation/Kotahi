1601e611a31daa98fd98a912d8bd8c70e355d564
Vukile Langa <vukilelanga@me.com>
2025-03-31 22:06:44 +0200

    refactor(models): simplify migration

--- packages/server/models/review/migrations/1738163837-add-is-imported-column.js
+++ packages/server/models/review/migrations/1738163837-add-is-imported-column.js
@@ -3,30 +3,21 @@ const Review = require('../review.model')
 
 exports.up = async knex => {
   return useTransaction(async trx => {
-    const hasIsImportedColumn = await knex.schema.hasColumn(
-      Review.tableName,
-      'is_imported',
-    )
+    await trx.schema.alterTable(Review.tableName, table => {
+      table.boolean('is_imported').notNullable().defaultTo('false')
+    })
 
-    if (!hasIsImportedColumn) {
-      await trx.schema.alterTable(Review.tableName, table => {
-        table.boolean('is_imported').notNullable().defaultTo('false')
-      })
+    const importedReviews = await Review.query(trx).where({ userId: null })
 
-      const importedReviews = await Review.query(trx)
-        .leftJoin('teams', 'teams.object_id', 'reviews.manuscript_id')
-        .where({ userId: null, role: null })
-
-      // mark reviews as imports.
-      // if no reviews were found, do not attempt to patch empty rows
-      if (importedReviews.length > 0) {
-        await Review.query(trx)
-          .whereIn(
-            'id',
-            importedReviews.map(r => r.id),
-          )
-          .patch({ isImported: true })
-      }
+    // mark reviews as imports.
+    // if no reviews were found, do not attempt to patch empty rows
+    if (importedReviews.length > 0) {
+      await Review.query(trx)
+        .whereIn(
+          'id',
+          importedReviews.map(r => r.id),
+        )
+        .patch({ isImported: true })
     }
   })
 }
