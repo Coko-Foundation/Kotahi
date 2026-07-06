36670deac54c846ef38b2e4f4caf63755d04a6eb
Vukile Langa <vukilelanga@me.com>
2026-07-06 18:02:35 +0200

    refactor(client): renamed default page to remove apfs ambiguity

--- packages/client/app/DefaultPage.jsx
+++ packages/client/app/DefaultPage.jsx
@@ -16,7 +16,7 @@ const modals = {
   assetManagerEditor: AssetManager,
 }
 
-const Pages = () => {
+const DefaultPage = () => {
   Modal.setAppElement('#root')
 
   return (
@@ -33,4 +33,4 @@ const Pages = () => {
   )
 }
 
-export default <Pages />
+export default <DefaultPage />
