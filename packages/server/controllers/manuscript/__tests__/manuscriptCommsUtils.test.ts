563e669de7aa62ec8bf284fb1990e7f35a824db1
yannis <yannisbarlas@gmail.com>
2026-07-20 16:03:08 +0300

    fix(*): make sure import job can handle archivePeriodDays missing from config

---
+++ packages/server/controllers/manuscript/__tests__/manuscriptCommsUtils.test.ts
@@ -0,0 +1,40 @@
+import { describe, beforeAll, beforeEach, afterAll, it, expect } from 'vitest'
+import { db, config, DbTestUtils, migrationManager } from '@coko/server'
+
+import Group from '../../../models/group/group.model'
+import Config from '../../../models/config/config.model'
+import { archiveOldManuscripts } from '../manuscriptCommsUtils'
+
+describe('archiveOldManuscripts', () => {
+  beforeAll(async () => {
+    await config.init()
+    db.init()
+    await migrationManager.migrate()
+  })
+
+  beforeEach(async () => {
+    await DbTestUtils.clearDb()
+  })
+
+  afterAll(async () => {
+    await db.destroy()
+  })
+
+  it('archivedOldManuscripts does not crash when archivePeriodDays is not set in the config', async () => {
+    const group = await Group.insert({})
+
+    await Config.insert({
+      active: true,
+      groupId: group.id,
+      formData: {
+        manuscript: {
+          // archivePeriodDays intentionally omitted,
+          // reproducing a config created before this field existed
+        },
+      },
+    })
+
+    const res = await archiveOldManuscripts(group.id)
+    expect(res).not.toBeDefined()
+  })
+})
