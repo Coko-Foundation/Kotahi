3b5ef0af68ec80bfeb099b9c662eeaa3965ed6db
Vukile Langa <vukilelanga@me.com>
2026-08-11 10:51:26 +0200

    fix(ui): remove group type capitalisation

--- packages/client/app/ui/base/Menu.tsx
+++ packages/client/app/ui/base/Menu.tsx
@@ -102,7 +102,6 @@ const GroupName = styled.div<{ $labelsWrap: boolean }>`
 
 const GroupType = styled.div`
   font-size: ${th('fontSizeBaseSmall')};
-  /* text-transform: capitalize; */
 `
 
 const Separator = styled.div`
